9d05cde769007f1443aef904ac64e7454792f939
Jake B <jbarber@gfxlabs.io>
2022-04-12 10:31:01 -0500

    fixed typechain errors
    Former-commit-id: 774cf7ef491cb853669fc14dab34d7c76bbfc5ea [formerly 12b7c6105e12f9b41bc4b5bc5bb28ed7dc1d1112]
    Former-commit-id: 3c73ca43a7905f6878ecaef5200ac7c1d3db87d8

--- typechain-types/lending/index.ts
+++ typechain-types/lending/index.ts
@@ -5,6 +5,4 @@ import type * as vaultSol from "./Vault.sol";
 export type { vaultSol };
 export type { IVault } from "./IVault";
 export type { IVaultController } from "./IVaultController";
-export type { IVaultMaster } from "./IVaultMaster";
 export type { VaultController } from "./VaultController";
-export type { VaultMaster } from "./VaultMaster";
