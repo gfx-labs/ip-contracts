92ea61e26727dde9d6a175d32b3cb970eb80c2df
Jake <jabot999@gmail.com>
2022-04-14 18:16:28 -0500

    Deployment is working for upgradeable USDi and VaultController
    Former-commit-id: 2ea8c11a268c0118e11ee0d508cc3debb32804fc [formerly ff746e28ccbd6a92ceb3c3732942da8ecb7098d6]
    Former-commit-id: dfe90355ff52b5b945d409ffc8c03e680a55012d

--- test/mainnet/9_upgradeable.ts
+++ test/mainnet/9_upgradeable.ts
@@ -3,6 +3,8 @@ import { ethers, upgrades } from "hardhat";
 import { expect, assert } from "chai";
 import { showBody } from "../util/format";
 import { BN } from "../util/number";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
 import { advanceBlockHeight, fastForward, mineBlock, OneWeek, OneYear } from "../util/block";
 
 import {
@@ -17,6 +19,9 @@ import {
     IOracleRelay,
     OracleMaster,
     OracleMaster__factory,
+    ProxyAdmin,
+    ProxyAdmin__factory,
+    TransparentUpgradeableProxy__factory,
     ThreeLines0_100,
     ThreeLines0_100__factory,
     UniswapV3OracleRelay__factory,
@@ -28,17 +33,82 @@ import {
     IVOTE,
     IVOTE__factory,
 } from "../../typechain-types";
+
+
+let VaultController: VaultController
+let USDi: USDI
+let ProxyController: ProxyAdmin
+
+let deployer: SignerWithAddress,
+    alice: SignerWithAddress,
+    bob: SignerWithAddress,
+    carol: SignerWithAddress,
+    dave: SignerWithAddress,
+    ethan: SignerWithAddress;
+let ganga: Array<SignerWithAddress>;
+
+const deployProxy = async () => {
+
+    [deployer, alice, bob, carol, dave, ethan] = await ethers.getSigners();
+    ganga = [deployer, alice, bob, carol, dave, ethan];
+    await mineBlock()
+
+
+    const uVC = await new VaultController__factory(deployer).connect(deployer).deploy()
+    await mineBlock()
+
+    ProxyController = await new ProxyAdmin__factory(deployer).connect(deployer).deploy()
+    await mineBlock()
+
+    let vc = await new TransparentUpgradeableProxy__factory(
+        deployer
+    ).connect(deployer).deploy(uVC.address, ProxyController!.address, "0x");
+    await mineBlock()
+
+    VaultController = await new VaultController__factory(deployer).attach(vc.address)
+    await mineBlock()
+    await VaultController.deployed()
+    await mineBlock()
+
+    await VaultController.initialize()
+    await mineBlock()
+
+
+    const uUSDi = await new USDI__factory(deployer).connect(deployer).deploy()
+    await mineBlock()
+    
+    let usd = await new TransparentUpgradeableProxy__factory(
+        deployer
+    ).connect(deployer).deploy(uUSDi.address, ProxyController!.address, "0x")
+    await mineBlock()
+
+    USDi = await new USDI__factory(deployer).attach(usd.address)
+    await mineBlock()
+    await USDi.deployed()
+    await mineBlock()
+    await USDi.initialize(s.usdcAddress)
+
+
+}
+
 describe("Test upgradeable", () => {
 
-    let vc: any
-    it("Deploy USDI core as Proxy`", async () => {
-        showBody("deploying usdi as proxy")
-        const bn = await ethers.provider.getBlockNumber()
-        showBody(bn)
-        let factory = await ethers.getContractFactory("VaultController")
-        vc = await upgrades.deployProxy(factory)
+    before(async () => {
+        await deployProxy()
+    })
+    it("Verify deployment of VaultController proxy", async () => {
         
+        const protocolFee = await VaultController.connect(alice).ProtocolFee()
+        await mineBlock()
+        const expectedProtocolFee = BN("1e14")
+        assert.equal(protocolFee.toString(), expectedProtocolFee.toString(), "VaultController Initialized")
 
     })
+    it("Verify deployment of USDi proxy", async () => {
+        const reserveAddress = await USDi._reserveAddress()
+        await mineBlock()
+        const expectedReserveAddress = s.usdcAddress
+        assert.equal(reserveAddress, expectedReserveAddress, "USDi Initialized")
+    })  
 
 })
