f218c33a3b88b8b00895fdefaf5927c385212aaf
Jake <jabot999@gmail.com>
2022-04-15 12:51:19 -0500

    Fixed withdraw bug
    Former-commit-id: 857a64e382d829ee29232415637fde7784a783c7 [formerly cb1898cecdf24d6d1e86bb64e9d47c4866580e0f]
    Former-commit-id: 37e34cf4b198a74fdecb6103bcb53f448a3f5133

--- test/mainnet/8_USDI.ts
+++ test/mainnet/8_USDI.ts
@@ -4,24 +4,13 @@ import { expect, assert } from "chai";
 import { showBody } from "../util/format";
 import { BN } from "../util/number";
 import { advanceBlockHeight, fastForward, mineBlock, OneWeek, OneYear } from "../util/block";
-//TODO
 
-const ERC20ABI = require('../../scripts/erc20ABI.ts')
-const abi = new ERC20ABI()
-const USDC_Contract = new ethers.Contract(s.usdcAddress, abi.erc20ABI(), ethers.provider)
+
 const usdcAmount = BN("5000e6")
 
 describe("TOKEN-DEPOSITS", async () => {
-    //bob tries to borrow usdi against 10 eth as if eth is $100k
-    // remember bob has 10 eth
-    it(`bob should not be able to borrow 1e6 * 1e18 * ${s.Bob_WETH} usdi`, async () => {
-        await expect(s.VaultController.connect(s.Bob).borrow_usdi(1,
-            s.Bob_WETH.mul(BN("1e18")).mul(1e6),
-        )).to.be.revertedWith("account insolvent");
-    });
-
     it("check starting balance and deposit USDC", async () => {
-        const startingUSDCamount = await USDC_Contract.balanceOf(s.Dave.address)
+        const startingUSDCamount = await s.USDC.balanceOf(s.Dave.address)
         assert.equal(startingUSDCamount.toString(), s.Dave_USDC.toString(), "Starting USDC balance is correct")
 
         const startingUSDIamount = await s.USDI.balanceOf(s.Dave.address)
@@ -29,25 +18,38 @@ describe("TOKEN-DEPOSITS", async () => {
 
         //approve and deposit 5000 USDC
         
-        await USDC_Contract.connect(s.Dave).approve(s.USDI.address, usdcAmount)
+        await s.USDC.connect(s.Dave).approve(s.USDI.address, usdcAmount)
         const depositResult = await s.USDI.connect(s.Dave).deposit(usdcAmount)
         await mineBlock()
         const depositReceipt = await depositResult.wait()
         const depositArgs = depositReceipt.events![depositReceipt.events!.length - 1]
         assert.equal(depositArgs.event!, "Deposit", "correct event emitted")
         
+        let usdcBalance = await s.USDC.balanceOf(s.Dave.address)
+        assert.equal(usdcBalance.toString(), s.Dave_USDC.sub(usdcAmount).toString(), "Dave deposited USDC tokens")
+
         let usdiBalance = await s.USDI.balanceOf(s.Dave.address)
         assert.equal(usdiBalance.toString(), usdcAmount.mul(1e12).toString(), "USDi balance is correct")
 
     });
+
+    //fixed bug in withdraw
     it("redeem USDC for USDI", async () => {
-        const startingUSDCamount = await USDC_Contract.balanceOf(s.Dave.address)
+        const startingUSDCamount = await s.USDC.balanceOf(s.Dave.address)
         assert.equal(startingUSDCamount.toString(), s.Dave_USDC.sub(usdcAmount).toString(), "Starting USDC balance is correct")
     
         const startingUSDIamount = await s.USDI.balanceOf(s.Dave.address)
         assert.equal(startingUSDIamount.toString(), usdcAmount.mul(1e12).toString(), "Starting USDi balance is correct")
-    
+        
+        //await s.USDC.connect(s.Dave).approve(s.USDI.address, usdcAmount)
         const withdrawResult = await s.USDI.connect(s.Dave).withdraw(usdcAmount)
+        await mineBlock()
+
+        let usdcBalance = await s.USDC.balanceOf(s.Dave.address)
+        assert.equal(usdcBalance.toString(), s.Dave_USDC.toString(), "Dave redeemed all USDC tokens")
+
+        let usdiBalance = await s.USDI.balanceOf(s.Dave.address)
+        assert.equal(usdiBalance.toString(), "0", "USDi balance is correct")
     
     });
 });
