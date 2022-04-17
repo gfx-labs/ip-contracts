0b192ceba961207393c3f769c80e9468df5b5b00
Andy Choi <achoi@gfxlabs.io>
2022-04-17 23:11:21 +0000

    Updated kink indexes and imposed that the third segment is flat
    Former-commit-id: 5f4be52b6101e75697d8177bd382765ef8655a1f [formerly a678956f562d997b525f05edaecb626ffa729629] [formerly 53ce27ee19094da27059d78472347a215121d6c3 [formerly ade52a9a55f9301ecd226eab4ec7440c3ed92e64]]
    Former-commit-id: c6988ef45835ffe4db4e7317d58c2586271266cb [formerly dd0b63af2784baa5fff41258d51b72ead4b80786]
    Former-commit-id: 5c79b73c194c482f9741551e241512a964b50163

--- contracts/curve/Logic/ThreeLines0_100.sol
+++ contracts/curve/Logic/ThreeLines0_100.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "../ICurveSlave.sol";
 
 // this is the piecewise linear function which returns values for input values 0 to 100
-// the variables _r0, _r1, _r2, and _r3, along with _s0 and _s1 describe the piecewise function
+// the variables _r0, _r1, and _r2, along with _s1 and _s2 describe the piecewise function
 //
 // the function generates appears as below
 //
@@ -27,64 +27,54 @@ import "../ICurveSlave.sol";
 ///      |                        \
 ///      |                         -\
 ///      |                          ***----\
-///      |                     (_s0, _r1)   ------------\
-///      |                                               -----------\ (_s1, _r2)
-///      |                                                           ----***-\
-///      |                                                                    ----\
-///      |                                                                         -----\
-///      |                                                                               ----\
-///      |                                                                                    ----\
-///      |                                                                                         ----\
-///      |                                                                                              -----\
-///      |                                                                                                    ----\
-///      |                                                                                                         ----\
-///      |                                                                                                              ----\
-///      |                                                                                                                   -----\
-///      |                                                                                                                         ----\
+///      |                     (_s1, _r1)   ----\
+///      |                                       ----\
+///      |                                            ----\   
+///      |                                                 ----\ (_s2, _r2)
+///      |                                                             ***--------------------------------------------------------------\
+///      |                                                                  
+///      |                                                                         
+///      |                                                                               
+///      |                                                                                    
 ///      +---------------------------------------------------------------------------------------------------------------------------------
-/// (0,0)                                                                                                                            (100, _r3)
+/// (0,0)                                                                                                                            (100, _r2)
 contract ThreeLines0_100 is ICurveSlave {
     int256 public _r0;
     int256 public _r1;
     int256 public _r2;
-    int256 public _r3;
-    int256 public _s0;
     int256 public _s1;
+    int256 public _s2;
 
     constructor(
         int256 r0,
         int256 r1,
         int256 r2,
-        int256 r3,
-        int256 s0,
-        int256 s1
+        int256 s1,
+        int256 s2
     ) {
         _r0 = r0;
         _r1 = r1;
         _r2 = r2;
-        _r3 = r3;
-        _s0 = s0;
         _s1 = s1;
+        _s2 = s2;
     }
 
     function valueAt(int256 x_value) external view override returns (int256) {
         int256 max = 1e18;
         require(x_value >= 0, "too small");
         require(x_value <= max, "too large");
-        if (x_value < _s0) {
+        if (x_value < _s1) {
             int256 rise = _r1 - _r0;
-            int256 run = _s0;
+            int256 run = _s1;
             return linearInterpolation(rise, run, x_value, _r0);
         }
-        if (x_value < _s1) {
+        if (x_value < _s2) {
             int256 rise = _r2 - _r1;
-            int256 run = _s1 - _s0;
+            int256 run = _s2 - _s1;
             return linearInterpolation(rise, run, x_value - _s0, _r1);
         }
         if (x_value <= max) {
-            int256 rise = _r3 - _r2;
-            int256 run = max - _s1;
-            return linearInterpolation(rise, run, x_value - _s1, _r2);
+            return _r2;
         }
         revert();
     }
