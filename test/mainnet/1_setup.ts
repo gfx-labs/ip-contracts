ec6169bafb2cf8b55baaaa7696b6418904d49c8c
a <a@tuxpa.in>
2022-04-18 10:45:31 -0500

    ok there
    Former-commit-id: c7d92624ac36902d4b55a3807478dc13e813e382

--- test/mainnet/1_setup.ts
+++ test/mainnet/1_setup.ts
@@ -8,7 +8,7 @@ import { advanceBlockHeight, reset, mineBlock } from "../util/block";
 import { IERC20__factory, IVOTE__factory } from "../../typechain-types";
 //import { assert } from "console";
 
-require('chai').should()
+require("chai").should();
 //*
 // Initial Balances:
 // Andy: 100,000,000 usdc ($100) 6dec
@@ -27,72 +27,50 @@ let usdc_minter = "0xe78388b4ce79068e89bf8aa7f218ef6b9ab0e9d0";
 let comp_minter = "0xf977814e90da44bfa03b6295a0616a897441acec";
 let weth_minter = "0xe78388b4ce79068e89bf8aa7f218ef6b9ab0e9d0";
 
-let carol_voting_address = "0x1F2AB8Ac759Fb0E3185630277A554Ae3110bF530"
+let carol_voting_address = "0x1F2AB8Ac759Fb0E3185630277A554Ae3110bF530";
 
 describe("hardhat settings", () => {
-    it("reset hardhat network each run", async () => {
-        expect(await reset()).to.not.throw
-    })
-    it("set automine OFF", async () => {
-        expect(await network.provider.send("evm_setAutomine", [false])).to.not.throw
-    })
-})
+  it("reset hardhat network each run", async () => {
+    expect(await reset()).to.not.throw;
+  });
+  it("set automine OFF", async () => {
+    expect(await network.provider.send("evm_setAutomine", [false])).to.not
+      .throw;
+  });
+});
 
 describe("Token Setup", () => {
-    it("connect to signers", async () => {
-        let accounts = await ethers.getSigners();
-        s.Frank = accounts[0];
-        s.Andy = accounts[6];
-        s.Bob = accounts[7];
-        s.Carol = accounts[8];
-        s.Dave = accounts[9];
-    })
-    it("Connect to existing contracts", async () => {
+  it("connect to signers", async () => {
+    let accounts = await ethers.getSigners();
+    s.Frank = accounts[0];
+    s.Andy = accounts[6];
+    s.Bob = accounts[7];
+    s.Carol = accounts[8];
+    s.Dave = accounts[9];
+  });
+  it("Connect to existing contracts", async () => {
+    s.USDC = IERC20__factory.connect(s.usdcAddress, s.Frank);
+    s.WETH = IERC20__factory.connect(s.wethAddress, s.Frank);
+    s.COMP = IVOTE__factory.connect(s.compAddress, s.Frank);
+  });
+  it("Should succesfully transfer money", async () => {
+    showBody(`stealing ${s.Andy_USDC} to andy from ${s.usdcAddress}`);
+    await expect(
+      stealMoney(usdc_minter, s.Andy.address, s.usdcAddress, s.Andy_USDC)
+    ).to.not.be.reverted;
+    showBody(`stealing ${s.Dave_USDC} to dave from ${s.usdcAddress}`);
+    await expect(
+      stealMoney(usdc_minter, s.Dave.address, s.usdcAddress, s.Dave_USDC)
+    ).to.not.be.reverted;
+    showBody(`stealing ${s.Carol_COMP} to carol from ${s.compAddress}`);
+    await expect(
+      stealMoney(comp_minter, s.Carol.address, s.compAddress, s.Carol_COMP)
+    ).to.not.be.reverted;
+    showBody(`stealing ${s.Bob_WETH} to bob from ${s.wethAddress}`);
+    await expect(
+      stealMoney(weth_minter, s.Bob.address, s.wethAddress, s.Bob_WETH)
+    ).to.not.be.reverted;
 
-        s.USDC = IERC20__factory.connect(
-            s.usdcAddress,
-            s.Frank
-        )
-        s.WETH = IERC20__factory.connect(
-            s.wethAddress,
-            s.Frank
-        )
-        s.COMP = IVOTE__factory.connect(
-            s.compAddress,
-            s.Frank
-        )
-    })
-    it("Should succesfully transfer money", async () => {
-
-        showBody(`stealing ${s.Andy_USDC} to andy from ${s.usdcAddress}`)
-        await expect(stealMoney(
-            usdc_minter,
-            s.Andy.address,
-            s.usdcAddress,
-            s.Andy_USDC
-        )).to.not.be.reverted
-        showBody(`stealing ${s.Dave_USDC} to dave from ${s.usdcAddress}`)
-        await expect(stealMoney(
-            usdc_minter,
-            s.Dave.address,
-            s.usdcAddress,
-            s.Dave_USDC
-        )).to.not.be.reverted
-        showBody(`stealing ${s.Carol_COMP} to carol from ${s.compAddress}`)
-        await expect(stealMoney(
-            comp_minter,
-            s.Carol.address,
-            s.compAddress,
-            s.Carol_COMP
-        )).to.not.be.reverted
-        showBody(`stealing ${s.Bob_WETH} to bob from ${s.wethAddress}`)
-        await expect(stealMoney(
-            weth_minter,
-            s.Bob.address,
-            s.wethAddress,
-            s.Bob_WETH
-        )).to.not.be.reverted
-
-        await mineBlock()
-    })
-})
+    await mineBlock();
+  });
+});
