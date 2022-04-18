eee505dc207dd16f87ed0a9fab942d0d9fe099d2
Jake B <jbarber@gfxlabs.io>
2022-04-18 12:26:18 -0500

    fixed borrowUSDi to be more preecise and adjusted tests
    Former-commit-id: 64a763db15b775694f12d3537f7f57e372aa893c [formerly 1a64f7c3d25d2a3cdbc7b333ae54152a3e4f92e5]
    Former-commit-id: 9c63c37c5749607f8b29127591c097f0cdcd576c

--- test/math/5_vault.ts
+++ test/math/5_vault.ts
@@ -9,7 +9,7 @@ import { IVault__factory } from "../../typechain-types";
 
 describe("Vault setup:", () => {
     it("mint vaults", async () => {
-        showBody("bob mint vault")
+        //showBody("bob mint vault")
         await expect(s.VaultController.connect(s.Bob).mintVault()).to.not.reverted;
         await mineBlock();
         let bobVault = await s.VaultController.VaultAddress(1)
@@ -20,7 +20,7 @@ describe("Vault setup:", () => {
         expect(await s.BobVault.Minter()).to.eq(s.Bob.address)
 
 
-        showBody("carol mint vault")
+        //showBody("carol mint vault")
         await expect(s.VaultController.connect(s.Carol).mintVault()).to.not.reverted;
         await mineBlock()
         let carolVault = await s.VaultController.VaultAddress(2)
@@ -35,17 +35,17 @@ describe("Vault setup:", () => {
         await expect(s.COMP.connect(s.Carol).transfer(s.CarolVault.address, s.Carol_COMP)).to.not.reverted;
         await mineBlock();
 
-        showBody("bob transfer weth")
+        //showBody("bob transfer weth")
         expect(await s.BobVault.tokenBalance(s.wethAddress)).to.eq(s.Bob_WETH)
 
-        showBody("carol transfer comp")
+        //showBody("carol transfer comp")
         expect(await s.CarolVault.tokenBalance(s.compAddress)).to.eq(s.Carol_COMP)
     })
     it("carol should be able to delegate votes", async () => {
         await expect(s.CarolVault.delegateCompLikeTo(s.compVotingAddress, s.compAddress)).to.not.reverted;
         await mineBlock();
         const currentVotes = await s.COMP.connect(s.Carol).getCurrentVotes(s.compVotingAddress);
-        showBody("carol should have", currentVotes, "votes");
+        //showBody("carol should have", currentVotes, "votes");
         expect(currentVotes).to.eq(s.Carol_COMP);
     });
 })
