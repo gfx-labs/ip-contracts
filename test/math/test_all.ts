550979cde6dad35d2f7b4969621c049b4477aeeb
Jake B <jbarber@gfxlabs.io>
2022-04-18 11:13:13 -0500

    added math folder for testing
    Former-commit-id: 4995fbb105768b339bf03c542e812dd1f0c583da [formerly dd5121628f8b07a55157bb4b047f32e4602efce9]
    Former-commit-id: 56db2be957458c143f4fa7863576bb46d8e7b4f3

---
+++ test/math/test_all.ts
@@ -0,0 +1,8 @@
+export * from "./1_setup"
+export * from "./2_deploy"
+export * from "./3_oracle"
+export * from "./4_curve"
+export * from "./5_vault"
+export * from "./6_minting"
+export * from "./7_lending"
+
