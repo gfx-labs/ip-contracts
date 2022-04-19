9cb23e9644349b528e35d6e341ea7c8437462d51
Jake B <jbarber@gfxlabs.io>
2022-04-19 16:01:00 -0500

    Switched to proxy deployment for math
    Former-commit-id: e4b149c4f851e80b7e27ab5c3df3c7cee682190e

--- test/math/2_deploy.ts
+++ test/math/2_deploy.ts
@@ -1,5 +1,5 @@
 import { s } from "./scope";
-import { upgrades } from "hardhat";
+import { upgrades, ethers } from "hardhat";
 import { expect, assert } from "chai";
 import { showBody } from "../util/format";
 import { BN } from "../util/number";
@@ -15,6 +15,9 @@ import {
     IOracleRelay,
     OracleMaster,
     OracleMaster__factory,
+    ProxyAdmin,
+    ProxyAdmin__factory,
+    TransparentUpgradeableProxy__factory,
     ThreeLines0_100,
     ThreeLines0_100__factory,
     UniswapV3OracleRelay__factory,
@@ -26,38 +29,98 @@ import {
     IVOTE,
     IVOTE__factory,
 } from "../../typechain-types";
-import { mineBlock } from "../util/block";
+import { advanceBlockHeight, fastForward, mineBlock, OneWeek, OneYear } from "../util/block";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+
+let ProxyController: ProxyAdmin
+
+
+
+const deployProxy = async () => {
+
+    await mineBlock()
+    const uVC = await new VaultController__factory(s.Frank).connect(s.Frank).deploy()
+    await mineBlock()
+
+    ProxyController = await new ProxyAdmin__factory(s.Frank).connect(s.Frank).deploy()
+    await mineBlock()
+
+    let vc = await new TransparentUpgradeableProxy__factory(
+        s.Frank
+    ).connect(s.Frank).deploy(uVC.address, ProxyController!.address, "0x");
+    await mineBlock()
+
+    s.VaultController = await new VaultController__factory(s.Frank).attach(vc.address)
+    await mineBlock()
+    await s.VaultController.deployed()
+    await mineBlock()
+
+    await s.VaultController.initialize()
+    await mineBlock()
+
+    const uUSDi = await new USDI__factory(s.Frank).connect(s.Frank).deploy()
+    await mineBlock()
+
+    let usd = await new TransparentUpgradeableProxy__factory(
+        s.Frank
+    ).connect(s.Frank).deploy(uUSDi.address, ProxyController!.address, "0x")
+    await mineBlock()
+
+    s.USDI = await new USDI__factory(s.Frank).attach(usd.address)
+    await mineBlock()
+    await s.USDI.deployed()
+    await mineBlock()
+    await s.USDI.initialize(s.usdcAddress)
+
+    let owner = await s.USDI.owner()
+    showBody("OWNER: ", owner)
+    showBody("Frank: ", s.Frank.address)
+
+    //await expect(s.USDI.owner()).to.equal(s.Frank.address)
+    showBody("vault controller set vault owner")
+    await s.USDI.setVaultController(s.VaultController.address)
+    //await expect(s.USDI.setVaultController(s.VaultController.address)).to.not.reverted
+    await mineBlock()
+}
 
 
 require('chai').should()
-
 describe("Deploy Contracts", () => {
-
-    it("Deploy USDI core", async () => {
-        //showBody("deploying usdi")
-        s.USDI = await new USDI__factory(s.Frank).deploy()
-        await mineBlock()
-        await s.USDI.initialize(s.usdcAddress)
-        await mineBlock()
-        expect(await s.USDI.owner()).to.equal(s.Frank.address)
-
-        //showBody("set usdi owner")
-        await expect(s.USDI.connect(s.Frank).setMonetaryPolicy(s.Frank.address)).to.not.reverted
-        await mineBlock()
-        expect(await s.USDI.monetaryPolicy()).to.equal(s.Frank.address)
-
-        //showBody("deploying vault controller")
-        await mineBlock()
-        s.VaultController = await new VaultController__factory(s.Frank).deploy();
-        await mineBlock()
-        let result = await s.VaultController.initialize()
-        await mineBlock()
-
-        expect(await s.USDI.owner()).to.equal(s.Frank.address)
-        //showBody("vault controller set vault owner")
-        await expect(s.USDI.setVaultController(s.VaultController.address)).to.not.reverted
-        await mineBlock()
+    before(async () => {
+        await deployProxy()
     })
+    it("Verify deployment of VaultController proxy", async () => {
+
+        const protocolFee = await s.VaultController.connect(s.Andy).ProtocolFee()
+        await mineBlock()
+        const expectedProtocolFee = BN("1e14")
+        assert.equal(protocolFee.toString(), expectedProtocolFee.toString(), "VaultController Initialized")
+
+    })
+    it("Verify deployment of USDi proxy", async () => {
+        const reserveAddress = await s.USDI._reserveAddress()
+        await mineBlock()
+        const expectedReserveAddress = s.usdcAddress
+        assert.equal(reserveAddress, expectedReserveAddress, "USDi Initialized")
+    })
+    describe("Sanity check USDI deploy", () => {
+        it("Should return the right name, symbol, and decimals", async () => {
+            expect(await s.USDI.name()).to.equal("USDI Token");
+            expect(await s.USDI.symbol()).to.equal("USDI");
+            expect(await s.USDI.decimals()).to.equal(18);
+            expect(await s.USDI.owner()).to.equal(s.Frank.address)
+        });
+        it(`The contract creator should have ${BN("1e18").toLocaleString()} fragment`, async () => {
+            expect(await s.USDI.balanceOf(await s.Frank.getAddress())).to.eq(BN("1e18"));
+        });
+        it(`the totalSupply should be ${BN("1e18").toLocaleString()}`, async () => {
+            expect(await s.USDI.totalSupply()).to.eq(BN("1e18"));
+        });
+        it("the owner should be the Frank", async () => {
+            expect(await s.USDI.owner()).to.eq(await s.Frank.getAddress());
+        });
+    });
 
     it("Deploy Curve", async () => {
         await mineBlock()
@@ -86,39 +149,39 @@ describe("Deploy Contracts", () => {
 
     it("Deploy Oracles", async () => {
         s.Oracle = await new OracleMaster__factory(s.Frank).deploy();
-        //showBody("set vault oraclemaster")
+        showBody("set vault oraclemaster")
         await expect(s.VaultController.connect(s.Frank).register_oracle_master(
             s.Oracle.address
         )).to.not.reverted;
 
-        //showBody("create uniswap comp relay")
+        showBody("create uniswap comp relay")
         s.UniswapRelayCompUsdc = await new UniswapV3OracleRelay__factory(
             s.Frank
         ).deploy(s.usdcCompPool, true, BN("1e12"), BN("1"));
         await mineBlock()
         expect(await s.UniswapRelayCompUsdc.currentValue()).to.not.eq(0)
 
-        //showBody("create uniswap eth relay")
+        showBody("create uniswap eth relay")
         s.UniswapRelayEthUsdc = await new UniswapV3OracleRelay__factory(
             s.Frank
         ).deploy(s.usdcWethPool, true, BN("1e12"), BN("1"));
         await mineBlock()
 
-        //showBody("create chainlink comp relay")
+        showBody("create chainlink comp relay")
         s.ChainlinkComp = await new ChainlinkOracleRelay__factory(s.Frank).deploy(
             "0xdbd020caef83efd542f4de03e3cf0c28a4428bd5", BN("1e10"), BN("1")
         );
         await mineBlock()
         expect(await s.ChainlinkComp.currentValue()).to.not.eq(0)
 
-        //showBody("create chainlink eth relay")
+        showBody("create chainlink eth relay")
         s.ChainlinkEth = await new ChainlinkOracleRelay__factory(s.Frank).deploy(
             "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419", BN("1e10"), BN("1")
         )
         await mineBlock()
         expect(await s.ChainlinkEth.currentValue()).to.not.eq(0)
 
-        //showBody("create COMP anchoredview")
+        showBody("create COMP anchoredview")
         s.AnchoredViewComp = await new AnchoredViewRelay__factory(s.Frank).deploy(
             s.UniswapRelayCompUsdc.address,
             s.ChainlinkComp.address,
@@ -128,7 +191,7 @@ describe("Deploy Contracts", () => {
         await mineBlock()
         expect(await s.AnchoredViewComp.currentValue()).to.not.eq(0)
 
-        //showBody("create ETH anchoredview")
+        showBody("create ETH anchoredview")
         s.AnchoredViewEth = await new AnchoredViewRelay__factory(s.Frank).deploy(
             s.UniswapRelayEthUsdc.address,
             s.ChainlinkEth.address,
@@ -141,25 +204,25 @@ describe("Deploy Contracts", () => {
     })
 
     it("Set vault oracles and CFs", async () => {
-        //showBody("set vault COMP oracle to anchored view")
+        showBody("set vault COMP oracle to anchored view")
         await expect(s.Oracle.connect(s.Frank).set_relay(
             s.compAddress,
             s.AnchoredViewComp.address
         )).to.not.reverted;
 
-        //showBody("set vault ETH oracle to anchored view")
+        showBody("set vault ETH oracle to anchored view")
         await s.Oracle.connect(s.Frank).set_relay(
             s.wethAddress,
             s.AnchoredViewEth.address,
         );
-        //showBody("register weth")
+        showBody("register weth")
         await expect(s.VaultController.connect(s.Frank).register_erc20(
             s.wethAddress,
             BN("5e17"),
             s.wethAddress,
             BN("5e16"),
         )).to.not.reverted;
-        //showBody("register comp")
+        showBody("register comp")
         await expect(s.VaultController.connect(s.Frank).register_erc20(
             s.compAddress,
             BN("4e17"),
@@ -167,26 +230,11 @@ describe("Deploy Contracts", () => {
             BN("5e16"),
         )).to.not.reverted;
 
-        //showBody("register vaultcontroller usdi")
+        showBody("register vaultcontroller usdi")
         await expect(s.VaultController.connect(s.Frank)
             .register_usdi(s.USDI.address)).to.not.reverted
         await mineBlock()
     })
 })
 
-describe("Sanity check USDI deploy", () => {
-    it("Should return the right name, symbol, and decimals", async () => {
-        expect(await s.USDI.name()).to.equal("USDI Token");
-        expect(await s.USDI.symbol()).to.equal("USDI");
-        expect(await s.USDI.decimals()).to.equal(18);
-    });
-    it(`The contract creator should have ${BN("1e18").toLocaleString()} fragment`, async () => {
-        expect(await s.USDI.balanceOf(await s.Frank.getAddress())).to.eq(BN("1e18"));
-    });
-    it(`the totalSupply should be ${BN("1e18").toLocaleString()}`, async () => {
-        expect(await s.USDI.totalSupply()).to.eq(BN("1e18"));
-    });
-    it("the owner should be the Frank", async () => {
-        expect(await s.USDI.owner()).to.eq(await s.Frank.getAddress());
-    });
-});
+
