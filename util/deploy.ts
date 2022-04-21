f17a33e9789d99850dbbf04dfc0f37dbfef667c2
eddy lee <eddy@gfxlabs.io>
2022-04-21 04:10:16 +0000

    ending it all
    Former-commit-id: e847b47b0116d96c9c451211cb38d3dcc890ce19 [formerly 81cc50ad1d472cb4e4bd00063e190e5af216655c]
    Former-commit-id: 21785944f4039efe5298fa75c4dad965aef6a7f6

--- util/deploy.ts
+++ util/deploy.ts
@@ -1,9 +1,18 @@
 import { Signer, utils, Contract, ContractFactory, Overrides } from "ethers";
+import { config, tenderly } from "hardhat";
+import hh from "hardhat"
 import { Proxy, ProxyAdmin, TransparentUpgradeableProxy__factory, OwnableUpgradeable } from "../typechain-types"
 
 import { advanceBlockHeight, fastForward, mineBlock, OneWeek, OneYear } from "./block";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { TenderlyContract } from "@tenderly/hardhat-tenderly/dist/tenderly/types";
 
-export const DeployContractWithProxy = async (factory: ContractFactory, deployer: Signer, admin: ProxyAdmin, ...args: any[]): Promise<any> => {
+export const DeployContractWithProxy = async (
+    factory: ContractFactory,
+    deployer: Signer,
+    admin: ProxyAdmin,
+    ...args: any[]
+): Promise<any> => {
     const uVC = await factory.connect(deployer).deploy()
     let vc = await new TransparentUpgradeableProxy__factory(deployer).connect(deployer).deploy(uVC.address, admin.address, "0x");
     await mineBlock()
@@ -11,6 +20,13 @@ export const DeployContractWithProxy = async (factory: ContractFactory, deployer
     await mineBlock()
     const con = factory.attach(vc.address)
     await con.initialize(...args)
+
+    //    await tenderly.network().verify({
+    //        name: con.,
+    //        address: con.address
+    //    })
+
+
     return mineBlock().then(() => { return con })
 }
 
