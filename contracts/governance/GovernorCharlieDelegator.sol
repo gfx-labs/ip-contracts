e940f5548804f9ac12f5ed0648beff39181f24ef
Getty Hill <getty@gfxlabs.io>
2022-04-22 03:56:58 +0000

    started on gov charlie
    Former-commit-id: daf845c2e685b8380ee4fbb8e0f7ff3e96e886b0 [formerly c79c4684822a5a52231dc4f56ad2bb88892c1679] [formerly 26f7e306ca8b1c7d6cf2b29771501684f77a8715 [formerly b08e34c7dd52ab1ae770adf00bfafd9044021890]]
    Former-commit-id: ee0cde7f97514d5351f324375963829cab431090
    Former-commit-id: 750874c8dd27f4da73ae33a1db3c3348d3118c42

---
+++ contracts/governance/GovernorCharlieDelegator.sol
@@ -0,0 +1,77 @@
+pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
+
+import "./GovernorCharlieInterfaces.sol";
+
+contract GovernorCharlieDelegator is GovernorCharlieDelegatorStorage, GovernorCharlieEvents {
+	constructor(
+			address ipt_,
+	        address implementation_,
+	        uint votingPeriod_,
+	        uint votingDelay_,
+            uint proposalThreshold_,
+            uint delay_, 
+            uint emergencyQuorumVotes_,
+            uint quorumVotes_) public {
+
+        delegateTo(implementation_, abi.encodeWithSignature("initialize(address,uint256,uint256,uint256)",
+                                                            ipt_,
+                                                            votingPeriod_,
+                                                            votingDelay_,
+                                                            proposalThreshold_,
+                                                            delay_,
+                                                            emergencyQuorumVotes_,
+                                                            quorumVotes_));
+
+        _setImplementation(implementation_);
+	}
+
+
+	/**
+     * @notice Called by itself via governance to update the implementation of the delegator
+     * @param implementation_ The address of the new implementation for delegation
+     */
+    function _setImplementation(address implementation_) public {
+        require(msg.sender == address(this), "GovernorCharlieDelegator::_setImplementation: can only be changed by calling itself");
+        require(implementation_ != address(0), "GovernorCharlieDelegator::_setImplementation: invalid implementation address");
+
+        address oldImplementation = implementation;
+        implementation = implementation_;
+
+        emit NewImplementation(oldImplementation, implementation);
+    }
+
+    /**
+     * @notice Internal method to delegate execution to another contract
+     * @dev It returns to the external caller whatever the implementation returns or forwards reverts
+     * @param callee The contract to delegatecall
+     * @param data The raw data to delegatecall
+     */
+    function delegateTo(address callee, bytes memory data) internal {
+        (bool success, bytes memory returnData) = callee.delegatecall(data);
+        assembly {
+            if eq(success, 0) {
+                revert(add(returnData, 0x20), returndatasize)
+            }
+        }
+    }
+
+	/**
+     * @dev Delegates execution to an implementation contract.
+     * It returns to the external caller whatever the implementation returns
+     * or forwards reverts.
+     */
+    function () external payable {
+        // delegate all other functions to current implementation
+        (bool success, ) = implementation.delegatecall(msg.data);
+
+        assembly {
+              let free_mem_ptr := mload(0x40)
+              returndatacopy(free_mem_ptr, 0, returndatasize)
+
+              switch success
+              case 0 { revert(free_mem_ptr, returndatasize) }
+              default { return(free_mem_ptr, returndatasize) }
+        }
+    }
+}
