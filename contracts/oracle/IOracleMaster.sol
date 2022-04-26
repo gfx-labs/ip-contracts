b804cc2e62c714f654ced9fc263cde8b9971c36f
eddy lee <eddy@gfxlabs.io>
2022-04-26 06:55:43 +0000

    comment
    Former-commit-id: d1722bf8bb099ea21b16fe1889c3cf231879128e [formerly 0a13e9e47e11824ec579ab4b734dcc102018979b] [formerly 2c4813f5a51c711e009a9534d4241309b1716b11] [formerly c830af469d52fea2d7e422094954c68b98c0c6c8 [formerly 1bdc855104fc20e837f2ce856550fe62a923e584] [formerly 0a07d7fd5a62c880b686aedd7ebfe29d4f001a22 [formerly bbf3cff3f6f672abfffe9e389a8d9e4e7d6ea217]]]
    Former-commit-id: b824f6d6b889709e88fc0824ad4473720a84af63 [formerly b5a0ba6d596b8de534b71c1431c4be69a820cf5d] [formerly b4d51c4c72c4fc6b4d92b8e87eeced8d05a649af [formerly d615603545c328addcdc4bedc9970585c350803d]]
    Former-commit-id: bed9575200c6ae975cff03b2085fb9c49489e7ea
    Former-commit-id: a3147ea9022bcca5d8375249146f849329913ce8

--- contracts/oracle/IOracleMaster.sol
+++ contracts/oracle/IOracleMaster.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 /// @title OracleMaster Interface
 /// @notice Interface for interacting with OracleMaster
 interface IOracleMaster {
+  // calling function
   function getLivePrice(address token_address) external view returns (uint256);
 
   // admin functions
