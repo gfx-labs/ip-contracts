326fc3aa98d717fc600ec6048d4c80dd0ddb9ec3
eddy lee <eddy@gfxlabs.io>
2022-04-26 06:05:49 +0000

    formatting
    Former-commit-id: 64378e81e31904b6b503c4c19143b37e318b838b [formerly 23d69588b1b3507f5005e371b33baef0e7420aef] [formerly 2ae93e271b812045c2ad523d6261d689e492f402 [formerly 35713b57298b98a9a598f91e1969ad0ca5e5a08b]]
    Former-commit-id: bb5e1e7d945e81060f734d2be26e5cbc491ddc3c
    Former-commit-id: 7d472bc23375e06efa2e04fe03a5630004db7b66

--- contracts/oracle/OracleMaster.sol
+++ contracts/oracle/OracleMaster.sol
@@ -6,9 +6,8 @@ import "./IOracleRelay.sol";
 
 import "../_external/Ownable.sol";
 
-
 /// @title An addressbook for oracle relays
-/// @notice the oraclemaster is simply an addressbook of address->relay 
+/// @notice the oraclemaster is simply an addressbook of address->relay
 /// this is so that contracts may use the OracleMaster to call any registered relays.
 /// relays are individually pausable by owner, which disables the relay until reenabled
 contract OracleMaster is IOracleMaster, Ownable {
@@ -32,14 +31,14 @@ contract OracleMaster is IOracleMaster, Ownable {
   }
 
   /// @notice admin only, sets relay for a token address to the relay addres
-  /// @param token_address address of the token 
-  /// @param relay_address address of the relay 
+  /// @param token_address address of the token
+  /// @param relay_address address of the relay
   function set_relay(address token_address, address relay_address) public override onlyOwner {
     _relays[token_address] = relay_address;
   }
 
   /// @notice admin only, pauses relay for a token address
-  /// @param token_address address of the token 
+  /// @param token_address address of the token
   /// @param state boolean true to pause, false to unpause
   function pause_relay(address token_address, bool state) public override onlyOwner {
     _paused[token_address] = state;
