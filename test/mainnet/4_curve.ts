ef97f7db5c8a0bed75ae48a2fef846642b1ad48e
eddy lee <eddy@gfxlabs.io>
2022-04-26 06:53:32 +0000

    fixed curve master
    Former-commit-id: f21a798012baf51e8574e7ac0d62420d18bd7308 [formerly cc8a931899c1a2fe3329355c1c0fdc93c64647bf]
    Former-commit-id: 46d96c48787234ea26c9e05b0ed6ba618ab751c6

--- test/mainnet/4_curve.ts
+++ test/mainnet/4_curve.ts
@@ -3,16 +3,19 @@ import { ethers } from "hardhat";
 import { expect, assert } from "chai";
 import { showBody } from "../../util/format";
 import { BN } from "../../util/number";
+import { mineBlock } from "../../util/block";
 
 describe("curve-threelines:", () => {
-    it("test setting a new curve value ", async () => {
+    it("test setting a new curve", async () => {
         await expect(s.Curve.connect(s.Frank).set_curve(
             "0x0000000000000000000000000000000000000000",
             s.ThreeLines.address
         )).to.not.reverted;
+        await mineBlock();
     })
-    it("test getting curve from curve master via 0 addr", async () => {
-        let val = (await s.Curve.getValueAt("0x0000000000000000000000000000000000000000", "0")).div(1e14).toNumber() / (1e4)
+    it("test threelines math via curve master", async () => {
+        let val: any;
+        val = (await s.Curve.getValueAt("0x0000000000000000000000000000000000000000", "0")).div(1e14).toNumber() / (1e4)
         expect(val).to.eq(2);
         val = (await s.Curve.getValueAt("0x0000000000000000000000000000000000000000", BN("25e16"))).div(1e14).toNumber() / (1e4)
         expect(val).to.eq(1.025);
