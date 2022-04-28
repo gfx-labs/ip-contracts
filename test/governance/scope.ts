93540f09a9250ad7e71f35a7b19fe2b2ac9a6e23
Getty Hill <getty@gfxlabs.io>
2022-04-28 00:40:31 +0000

    basic gov tests down
    Former-commit-id: 28314bff52442ea498948435012dd3c3707c7ca4 [formerly e6e56a5b9176e14485948a5425f3ac178b6aab4f] [formerly bcc1dcb3ce59bf63d3d150b75963bad7c90e29d4 [formerly 704c13e4207030c47b7853175f10004a5006d67c]] [formerly 3f70a583a82e07091f493b6ae838a084b8243ee4 [formerly 7ac235776edc22bb564550eeb6b4bc8f0eb2f850] [formerly 5ea4c83a05024b9111e2b44c6cc5e88fca943e18 [formerly a8ce7fe98fcca09790014387fe45968fa8e437fe]]]
    Former-commit-id: d44d2380bc04a4cfdc7b36a2d65d5ae38ae44ff9 [formerly ca28d8ced8a3699e586de8c382ca0ac4b800ae61] [formerly 6cac04186f231c6b70094a80e4e1f16bf7719268]
    Former-commit-id: 3eaf64d9ff61865ec42eca613bc991099e7b9908 [formerly a9266f401e5834f70ba5a3ddeb3c2a658c1caeea]
    Former-commit-id: cf7d018f80279c532af43000787c089ab58b8435

--- test/governance/scope.ts
+++ test/governance/scope.ts
@@ -7,7 +7,7 @@ import { BN } from "../../util/number";
 export class TestScope extends MainnetAddresses {
     USDC!: IERC20;
 
-    Frank_USDC = BN("1e10")
+    Frank_USDC = BN("1e11")
 
     IPT!: InterestProtocolTokenDelegate;
 
@@ -22,6 +22,7 @@ export class TestScope extends MainnetAddresses {
     Frank!: SignerWithAddress  // frank holds all IPT from the mint
     Andy!: SignerWithAddress // frank sends andy some ipt
     Eric!: SignerWithAddress // frank delegates the ipt to eric
+    Bob!: SignerWithAddress // whitelisted
 
     constructor() {
         super()
