255bcea374d3d40f3e118d0530ca6844b6032d98
Jake <jabot999@gmail.com>
2022-04-28 13:17:57 -0500

    exported math functions
    Former-commit-id: 212cd9863e3f82be69d1887d2d51d939b9095c60 [formerly 742072ac854eb54c9586bb359377be27eb2cd320] [formerly 518c8a9cd38f5b3faca354e8c44646e555bb2279 [formerly c9ab0e1cc984561ab75659b6985d72b02ea38f29]]
    Former-commit-id: d2148b64b43d890244b951e7df1ae14795cdf735
    Former-commit-id: 0e8f5ed8096d938b6c8a79ca832d8de687614426

--- util/math.ts
+++ util/math.ts
@@ -1,40 +1,101 @@
 import { s } from "../test/mainnet/scope";
 import { BigNumber, Event, utils } from "ethers";
 import { ethers } from "hardhat"
-import { advanceBlockHeight, fastForward, mineBlock, OneWeek, OneYear } from "./block";
+import { advanceBlockHeight, nextBlockTime, fastForward, mineBlock, OneWeek, OneYear } from "./block";
 import { BN } from "./number";
 import { showBody } from "./format";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
 /**
  * @dev takes interest factor and returns new interest factor - pulls block time from network and latestInterestTime from contract
  * @param interestFactor  - current interest factor read from contract
  * @returns new interest factor based on time elapsed and reserve ratio (read from contract atm)
  */
-export const payInterestMath = async (interestFactor: BigNumber) => {
-
+ export const payInterestMath = async (interestFactor: BigNumber) => {
     const nullAddr = "0x0000000000000000000000000000000000000000"
 
-    console.log(s.VaultController.address)
+    /**
+     * get interest factor from contract
+     * //time passes
+     * run payInterestMath to get new interest factor
+     * calculate_interest()
+     * get interest factor from contract
+     * should match interestFactor calculated in payInterestMath
+     * after confirming they match, use calculated interest factor go calc balance, liability, etc
+     */
+
+    //let interestFactor = await s.VaultController.InterestFactor()
 
     const latestInterestTime = await s.VaultController.LastInterestTime()//calculate? 
     const currentBlock = await ethers.provider.getBlockNumber()
     const currentTime = (await ethers.provider.getBlock(currentBlock)).timestamp
-    let timeDifference = currentTime - latestInterestTime.toNumber() + 1 //account for change when fetching from provider
+    await nextBlockTime(currentTime)
+    //await network.provider.send("evm_mine")
+
+    let timeDifference = currentTime - latestInterestTime.toNumber() + 1 //new block must have time ++1
 
     const reserveRatio = await s.USDI.reserveRatio()//todo - calculate
     const curve = await s.Curve.getValueAt(nullAddr, reserveRatio)//todo - calculate
 
     let calculation = BN(timeDifference).mul(BN("1e18").mul(curve))//correct step 1
     calculation = calculation.div(OneYear)//correct step 2 - divide by OneYear
-    calculation = calculation.div(BN("1e18"))//truncate
+    calculation = await truncate(calculation)//truncate
     calculation = calculation.mul(interestFactor)
-    calculation = calculation.div(BN("1e18"))//truncate again
-
-    //showBody("Interest Factor increase: ", calculation)
-    //showBody("Provided Interest Factor: ", interestFactor)
-    //showBody("New Interest Factor: ", interestFactor.add(calculation))
+    calculation = await truncate(calculation)//truncate again
 
     //new interest factor
     return interestFactor.add(calculation)
+}
+/**
+ * @note proper procedure: read interest factor from contract -> elapse time -> call this to predict balance -> pay_interest() -> compare 
+ * @param interestFactor CURRENT interest factor read from contract before any time has elapsed
+ * @param user whose balance to calculate interest on? 
+ * @returns expected balance after pay_interest()
+ */
+ export const calculateBalance = async (interestFactor: BigNumber, user: SignerWithAddress) => {
+    const totalBaseLiability = await s.VaultController._totalBaseLiability()
+    const protocolFee = await s.VaultController._protocolFee()
+
+    let valueBefore = await truncate(totalBaseLiability.mul(interestFactor))
+
+    const calcInterestFactor = await payInterestMath(interestFactor)
+
+    let valueAfter = await truncate(totalBaseLiability.mul(calcInterestFactor))
+    const protocolAmount = await truncate((valueAfter.sub(valueBefore)).mul(protocolFee))
+
+    const donationAmount = valueAfter.sub(valueBefore).sub(protocolAmount)//wrong
+    const currentTotalSupply = await s.USDI.totalSupply()
+    let newSupply = currentTotalSupply.add(donationAmount)
+
+    //totalGons
+    const totalGons = await s.USDI._totalGons()
+
+    //gpf
+    const gpf = totalGons.div(newSupply)
+
+    //calculate balance 
+    //get gon balance - calculate? 
+    const gonBalance = await s.USDI.scaledBalanceOf(user.address)
+
+    const expectedBalance = gonBalance.div(gpf)
+    return expectedBalance
+}
+
+/**
+ * 
+ * @param borrowAmount original borrow amount
+ * @param currentInterestFactor current interest factor read from contract 
+ * @param initialInterestFactor original interest factor from when borrow took place
+ * @returns 
+ */
+ export const calculateAccountLiability = async (borrowAmount: BigNumber, currentInterestFactor: BigNumber, initialInterestFactor: BigNumber) => {
+
+    let baseAmount = borrowAmount.mul(BN("1e18"))
+    baseAmount = baseAmount.div(initialInterestFactor)
+    let currentLiability = baseAmount.mul(currentInterestFactor)
+    currentLiability = await truncate(currentLiability)
+
+    return currentLiability
 }
 
 export const getGas = async (result: any) => {
