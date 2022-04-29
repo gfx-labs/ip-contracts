b611b1bdc872dc788122732fc1499586fd943438
Getty Hill <getty@gfxlabs.io>
2022-04-29 20:56:33 +0000

    check the delegate function on a few coins
    Former-commit-id: ab31354fbd97167bff6f2a1011c6c7e58b91a76a [formerly 7ade0014c5b3c81fc64d2113414206c832811936] [formerly 4de4821678879206d580c69aebf75b268ad61977 [formerly acb4e3460dd46fe83810abb0342e9b3636227ccd]] [formerly 02dff787e3365d8ec5b5f4703b0db506909ea7f1 [formerly 96afefeb9ee12be426cf90c8ef91e8eddacdceb7] [formerly d6b249bf95c7bb7d4713bf9a42db2334b2674094 [formerly 03b9d2edcc86682f489a3ceefcf63e72f232c5f6]]]
    Former-commit-id: 998ac72ab15f354815bc636c7b950ee94b6a3255 [formerly de73abc2f156b149235cd3351eab24c9972b905d] [formerly 0f1316f81a3bbfacb3caf0b07b61340be132cfe1]
    Former-commit-id: c2e53836eab9c93d25166485b0e38ada4cad4bfb [formerly 6b69ea37ece7778b610122c02602987dd62ce13d]
    Former-commit-id: 34e40b5b5089fff852d3387053a4655b35d3d458

--- contracts/_external/IVOTE.sol
+++ contracts/_external/IVOTE.sol
@@ -7,10 +7,21 @@ import "./IERC20.sol";
  * @dev Interface of the ERC20 standard as defined in the EIP.
  */
 interface IVOTE is IERC20 {
+  enum DelegationType {VOTING_POWER, PROPOSITION_POWER}
+
   /**
    * @notice Gets the current votes balance for `account`
    * @param account The address to get votes balance
    * @return The number of current votes for `account`
    */
   function getCurrentVotes(address account) external view returns (uint96);
+
+  /**
+   * @dev returns the current delegated power of a user. The current power is the
+   * power delegated at the time of the last snapshot
+   * @param user the user
+   **/
+  function getPowerCurrent(address user, DelegationType delegationType) external view returns (uint256);
+
+  function getVotes(address account) external view returns (uint256);
 }
