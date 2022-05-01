7422e85d2c1219fea0db77e9b4fc14cef683cea2
eddy lee <eddy@gfxlabs.io>
2022-05-01 05:50:24 +0000

    fuck me
    Former-commit-id: 09a3eb3446cef0eab2b55da54aef5e71c673a59d [formerly 1080413f676bbcb3632e5b2a37ec8a8eb272d369] [formerly 59e3ca0347161cc2d975b95ae93c01a198384ba0 [formerly 7181134f3038293bd67c7961407e1884d8628645]]
    Former-commit-id: 04f649d6e645e9e0f9695672c2879c53c332da5c
    Former-commit-id: 1780d408ed6b2ea50795ca564d1af7ade1faa1cf

--- contracts/lending/IVaultController.sol
+++ contracts/lending/IVaultController.sol
@@ -1,79 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/// @title VaultController Interface
-/// @notice Interface for interacting with the VaultController
-interface IVaultController {
-  // view functions
-  function _totalBaseLiability() external view returns (uint192);
-
-  function _protocolFee() external view returns (uint192);
-
-  function LastInterestTime() external view returns (uint64);
-
-  function InterestFactor() external view returns (uint192);
-
-  function ProtocolFee() external view returns (uint192);
-
-  function VaultAddress(uint96 id) external view returns (address);
-
-  function AmountToSolvency(uint96 id) external view returns(uint256);
-
-  function AccountLiability(uint96 id) external view returns (uint192);
-
-  function AccountBorrowingPower(uint96 id) external view returns (uint192);
-
-  function TokensToLiquidate(uint96 id, address token) external view returns (uint256);
-
-  // methods
-  function initialize() external;
-
-  function mintVault() external returns (address);
-
-  function checkAccount(uint96 id) external view returns (bool);
-
-  function liquidate_account(
-    uint96 id,
-    address asset_address,
-    uint256 tokenAmount
-  ) external returns (uint256);
-
-  function borrowUsdi(uint96 id, uint192 amount) external;
-
-  function repayUSDi(uint96 id, uint192 amount) external;
-
-  function repayAllUSDi(uint96 id) external;
-
-  function calculateInterest() external returns (uint256);
-
-  // admin
-  function pause() external;
-
-  function unpause() external;
-
-  function register_oracle_master(address master_oracle_address) external;
-
-  function register_curve_master(address master_curve_address) external;
-
-  function change_protocol_fee(uint192 new_protocol_fee) external;
-
-  function register_erc20(
-    address token_address,
-    uint256 LTV,
-    address oracle_address,
-    uint256 liquidationIncentive
-  ) external;
-
-  function register_usdi(address usdi_address) external;
-
-  function update_registered_erc20(
-    address token_address,
-    uint256 LTV,
-    address oracle_address,
-    uint256 liquidationIncentive
-  ) external;
-
-  // events
+// @title VaultController Events
+/// @notice interface which contains any events which the VaultController contract emits
+interface VaultControllerEvents {
   event InterestEvent(uint64 epoch, uint192 amount, uint256 curve_val);
   event NewProtocolFee(uint256 protocol_fee);
   event RegisteredErc20(address token_address, uint256 LTVe4, address oracle_address, uint256 liquidationIncentivee4);
@@ -89,4 +19,61 @@ interface IVaultController {
   event BorrowUSDi(uint256 vaultId, address vaultAddress, uint256 borrowAmount);
   event RepayUSDi(uint256 vaultId, address vaultAddress, uint256 repayAmount);
   event Liquidate(uint256 vaultId, address asset_address, uint256 usdi_to_repurchase, uint256 tokens_to_liquidate);
+}
+
+/// @title VaultController Interface
+/// @notice extends VaultControllerEvents
+interface IVaultController is VaultControllerEvents{
+  // initializer
+  function initialize() external;
+
+  // view functions
+  function lastInterestTime() external view returns (uint64);
+  function interestFactor() external view returns (uint192);
+  function protocolFee() external view returns (uint192);
+  function vaultAddress(uint96 id) external view returns (address);
+  function amountToSolvency(uint96 id) external view returns(uint256);
+  function accountLiability(uint96 id) external view returns (uint192);
+  function accountBorrowingPower(uint96 id) external view returns (uint192);
+  function tokensToLiquidate(uint96 id, address token) external view returns (uint256);
+  function checkAccount(uint96 id) external view returns (bool);
+  function totalBaseLiability() external view returns (uint256);
+
+  // interest calculations
+  function calculateInterest() external returns (uint256);
+
+  // vault management business
+  function mintVault() external returns (address);
+  function liquidateAccount(
+    uint96 id,
+    address asset_address,
+    uint256 tokenAmount
+  ) external returns (uint256);
+
+  function borrowUsdi(uint96 id, uint192 amount) external;
+  function repayUSDi(uint96 id, uint192 amount) external;
+  function repayAllUSDi(uint96 id) external;
+
+
+  // admin
+  function pause() external;
+  function unpause() external;
+  function register_oracle_master(address master_oracle_address) external;
+  function register_curve_master(address master_curve_address) external;
+  function change_protocol_fee(uint192 new_protocol_fee) external;
+  function register_erc20(
+    address token_address,
+    uint256 LTV,
+    address oracle_address,
+    uint256 liquidationIncentive
+  ) external;
+  function register_usdi(address usdi_address) external;
+  function update_registered_erc20(
+    address token_address,
+    uint256 LTV,
+    address oracle_address,
+    uint256 liquidationIncentive
+  ) external;
+
+  
 }
