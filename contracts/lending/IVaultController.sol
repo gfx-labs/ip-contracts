8fffb6b05128c8091649866a7ea268627933b1f2
Jake <jabot999@gmail.com>
2022-05-01 17:53:56 -0500

    chenged totalBaseLiability to implicit view function in interface
    Former-commit-id: a32d4b3864e9eec9e3be221d01f63ecd206dafe0 [formerly 406fd8300eed78ccf078f7c96e059b34de51e407] [formerly 51f07cac51eb9d3e7bc76d2232a2d15fe3c1cd3f] [formerly 01f1ca0ffe80cf2a56ccc8cf07948f961dcc6cac [formerly 4381ce846f58f7ffc83aa658f286b7545810f87b] [formerly 601664486b340ccf6ec62c4ac053c635c29fdef4 [formerly 741ec02ff28c115f64ce28e00d5758133f11b752]]] [formerly d31bc551fc5b924f0ad20b7aed77fca8f6c134ce [formerly 360af9c3a86f361626de837c2aeef465c994fa3b] [formerly eb233b8f5776247d47ba13f9e3ed86b4f362e446] [formerly 23ee78c8a98e2cd847dc68daeee3a559f3789b0e [formerly 6114cfd07d0a5495e45574455da4e17d91ce2c59] [formerly d8598417b16f2d72602c2c51aaa452d3ed5e7570 [formerly 31b8558d8b961228aceaf55525b6dd641301959d]]]]
    Former-commit-id: d31bc551fc5b924f0ad20b7aed77fca8f6c134ce [formerly 360af9c3a86f361626de837c2aeef465c994fa3b] [formerly eb233b8f5776247d47ba13f9e3ed86b4f362e446]
    Former-commit-id: 51564603aeac84c1675d71cf53f0c1fbea5bbf8c [formerly 16b753008397fc3b7d5fdfef52672322591f1dd8] [formerly be3931fd74bbcc8089a700e32ea91e1879c7736e [formerly 60e59178d8a067d67dcd9ea4c05a75220d3b05ab]]
    Former-commit-id: 2a47b489ba3161c2435d0fe10c1b87e1bca9f0dd
    Former-commit-id: 966d05a2b377a3fb862af2c45084debc14191a42

--- contracts/lending/IVaultController.sol
+++ contracts/lending/IVaultController.sol
@@ -23,27 +23,38 @@ interface VaultControllerEvents {
 
 /// @title VaultController Interface
 /// @notice extends VaultControllerEvents
-interface IVaultController is VaultControllerEvents{
+interface IVaultController is VaultControllerEvents {
   // initializer
   function initialize() external;
 
+  //implicit view functions
+  function _totalBaseLiability() external view returns (uint192);
+
   // view functions
   function lastInterestTime() external view returns (uint64);
+
   function interestFactor() external view returns (uint192);
+
   function protocolFee() external view returns (uint192);
+
   function vaultAddress(uint96 id) external view returns (address);
-  function amountToSolvency(uint96 id) external view returns(uint256);
+
+  function amountToSolvency(uint96 id) external view returns (uint256);
+
   function accountLiability(uint96 id) external view returns (uint192);
+
   function accountBorrowingPower(uint96 id) external view returns (uint192);
+
   function tokensToLiquidate(uint96 id, address token) external view returns (uint256);
+
   function checkAccount(uint96 id) external view returns (bool);
-  function totalBaseLiability() external view returns (uint256);
 
   // interest calculations
   function calculateInterest() external returns (uint256);
 
   // vault management business
   function mintVault() external returns (address);
+
   function liquidateAccount(
     uint96 id,
     address asset_address,
@@ -51,29 +62,35 @@ interface IVaultController is VaultControllerEvents{
   ) external returns (uint256);
 
   function borrowUsdi(uint96 id, uint192 amount) external;
-  function repayUSDi(uint96 id, uint192 amount) external;
-  function repayAllUSDi(uint96 id) external;
 
+  function repayUSDi(uint96 id, uint192 amount) external;
+
+  function repayAllUSDi(uint96 id) external;
 
   // admin
   function pause() external;
+
   function unpause() external;
+
   function register_oracle_master(address master_oracle_address) external;
+
   function register_curve_master(address master_curve_address) external;
+
   function change_protocol_fee(uint192 new_protocol_fee) external;
+
   function register_erc20(
     address token_address,
     uint256 LTV,
     address oracle_address,
     uint256 liquidationIncentive
   ) external;
+
   function register_usdi(address usdi_address) external;
+
   function update_registered_erc20(
     address token_address,
     uint256 LTV,
     address oracle_address,
     uint256 liquidationIncentive
   ) external;
-
-  
 }
