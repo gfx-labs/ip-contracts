5efdac1ef38ff116851bd113ee3fb706f6480899
Jake <jabot999@gmail.com>
2022-05-01 18:10:05 -0500

    added changes
    Former-commit-id: bbea3bd701300ec892223389a83324a6ed0e1837 [formerly c4895c962cd7d5edae01268e2f08864e7bce23e4]
    Former-commit-id: 5c316599c2e13e6df83993faa0e73f0308d51db3

--- test/mainnet/7_lending.ts
+++ test/mainnet/7_lending.ts
@@ -724,5 +724,14 @@ describe("Testing remaining vault functions", () => {
         await mineBlock()
     })
 })
-
+describe("Checking getters", () => {
+    it("checks totalBaseLiability", async () => {
+        let _totalBaseLiability = await s.VaultController._totalBaseLiability()
+        expect(_totalBaseLiability).to.not.eq(0)
+    })
+    it("checks _tokensRegistered", async () => {
+        let _tokensRegistered = await s.VaultController._tokensRegistered()
+        expect(_tokensRegistered).to.not.eq(0)
+    })
+})
 
