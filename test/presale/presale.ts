658d5c77ed2b48662d4fb0cd5c1ccaa94ae39614
Jake B <jbarber@gfxlabs.io>
2022-05-03 14:53:42 -0500

    merkle proof is working
    Former-commit-id: 896c52616d3d856474f535571e75a773d21d07c8 [formerly 26bd4ac36610f1a2004f98f3308994b6b14066e7] [formerly 079be5b64f4f578fa2d3a14de14be6a80121400f [formerly caa4d43daf4b2ba3b2f64a3f923c7a9cda79e10e]]
    Former-commit-id: 6fc5a1293370e66ef0e2a04664e6de3e69a78f62
    Former-commit-id: 827ebc2ff6648003f9d743bc14756600289d5a6f

--- test/presale/presale.ts
+++ test/presale/presale.ts
@@ -1,31 +1,73 @@
 import { s } from "./scope";
 import { ethers } from "hardhat";
 import { BigNumber, utils } from "ethers";
+import { keccak256, solidityKeccak256 } from "ethers/lib/utils";
 
 import { expect, assert } from "chai";
 import { showBody } from "../../util/format";
-import {getArgs} from "../../util/math"
+import { getArgs } from "../../util/math"
 import { BN } from "../../util/number";
 import { currentBlock, advanceBlockHeight, fastForward, mineBlock, OneWeek, OneYear } from "../../util/block";
 import { Impersonate, stopImpersonate, Impersonator } from "../../util/impersonator"
 import { start } from "repl";
-import { treeFromObject, getAccountProof, createTree } from "../../util/wave"
+import { treeFromObject, getAccountProof, createTree, treeFromAccount } from "../../util/wave"
 import { sha256, sha224 } from 'js-sha256';
 import MerkleTree from "merkletreejs";
 
-const merkleWallets = require("../data/data.json")
+import { Wave } from "../../typechain-types"
 
-const merkletree = createTree();
-const root = merkletree.getHexRoot()
+//const merkleWallets = require("../data/data.json")
+const data = require("../data/data.json")
+//const merkletree = createTree();
 
+
+const keyAmount = BN("500e6")//500 USDC
+
+const initMerkle = async () => {
+    //8 accunts to make a simple merkle tree
+    whitelist = [
+        s.Frank.address,
+        s.Andy.address,
+        s.Bob.address,
+        s.Carol.address,
+        s.Dave.address,
+        s.Eric.address,
+        s.Gus.address,
+        s.Hector.address
+    ]
+
+
+
+    const leafNodes = whitelist.map(addr => solidityKeccak256(["address", "uint256"],[keccak256(addr), keyAmount]))
+    
+
+
+
+    merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true })
+    root = merkleTree.getHexRoot()
+
+    //merkletree = treeFromAccount(whitelist)
+
+    //showBody(merkleTree.toString())
+}
+
+
+let whitelist: string[]
+let root: string
+let merkleTree: MerkleTree
 const totalSupply_ = BN("1e26")
-let Wave: any
+let Wave: Wave
 describe("Deploy wave", () => {
+
+    before(async () => {
+        await initMerkle()
+    })
+
     it("deploys wave", async () => {
 
         //init constructor args
 
-        const totalReward = totalSupply_.div(4)
+        const totalClaimed = totalSupply_.div(4)
         const floor = BN("5e5")//500,000 - .5 USDC
         const block = await currentBlock()
         const enableTime = block.timestamp
@@ -34,21 +76,28 @@ describe("Deploy wave", () => {
         //showBody(s.Frank.address)
 
         const waveFactory = await ethers.getContractFactory("Wave")
-        Wave = await waveFactory.connect(s.Frank).deploy(root, totalReward, floor, enableTime, disableTime, receiver)
+        Wave = await waveFactory.deploy(root, totalClaimed, floor, enableTime, disableTime, receiver)
         await mineBlock()
         await Wave.deployed()
         await mineBlock()
     })
     it("Sanity check state of Wave contract", async () => {
-        const _enableTime = await Wave._enableTime()
-        showBody(_enableTime)
+        const merkleRoot = await Wave.merkleRoot()
+        assert.equal(merkleRoot.toString(), root, "Merkle root is correct")
 
-        const totalReward = await Wave._totalClaimed()
-        showBody(totalReward)
-        assert.equal(totalReward.toString(), totalSupply_.div(4).toString(), "Total reward is correct")
+        const totalClaimed = await Wave._totalClaimed()
+        //showBody(totalClaimed)
+        assert.equal(totalClaimed.toString(), totalSupply_.div(4).toString(), "Total reward is correct")
 
         const floor = await Wave._floor()
-        showBody(floor)
+        assert.equal(floor.toNumber(), BN("5e5").toNumber(), "Floor is correct")
+
+        const receiver = await Wave._receiver()
+        assert.equal(receiver, s.Frank.address, "receiver is correct")
+
+        const totalReward = await Wave._totalReward()
+        //showBody("totalReward: ", totalReward)
+
 
     })
 })
@@ -57,52 +106,87 @@ describe("Deploy wave", () => {
 describe("Presale", () => {
     it("getPoints", async () => {
 
-        //getPoints
-        const claimArray = Object.entries(merkleWallets);
-        //const addr = claimArray[2][0];
-        const amt = claimArray[3][1];
-        const convertMultiplier = BN(amt).div(BN("1e6"));
-        showBody("convertMultiplier: ", convertMultiplier)
-        //await Wave.getPoints(BN("100e6"), BN("100e12"), root)
+        
+        const claimer = s.Bob.address
+        const claimerHash = keccak256(claimer)
+        let leaf = solidityKeccak256(["bytes32", "uint256"], [claimerHash, keyAmount])
 
+        let merkleProof = merkleTree.getHexProof(leaf)
+        showBody("leaf proof: ", merkleProof)
+
+
+
+
+
+
+    })
+
+    it("redeem before time has elapsed", async () => {
+        /**
+         * 
+         * 
+         * 
+         * 
+    //getPoints
+
+        //const addr = claimArray[2][0];
+        //const key = claimArray[3][1];
+        const amount = BN("100e6")//100 USDC
         const startingClaimed = await Wave._totalClaimed()
 
-        const pointsResult = await Wave.connect(s.Bob)
+        let proof = merkleTree.getHexProof(solidityKeccak256(["address", "uint256"], [s.Andy.address, amount.toString()]))
+
+
+
+
+        const pointsResult = await Wave.connect(s.Andy)
             .getPoints(
-                convertMultiplier,
-                convertMultiplier,
-                getAccountProof(merkletree, s.Bob.address, convertMultiplier)
+                amount,
+                5,//key
+                proof
+                //getAccountProof(merkleTree, s.Andy.address, amount)
             )
         await mineBlock()
         //const pointsArgs = await getArgs(pointsResult)
         //showBody(pointsArgs)
 
         const endingClaimed = await Wave._totalClaimed()
-        showBody(startingClaimed)
-        showBody(endingClaimed)
+        //showBody(startingClaimed)
+        //showBody(endingClaimed)
 
-    })
 
-    it("redeem before time has elapsed", async () => {
-        let redeemedState = await Wave.redeemed(s.Bob.address)
-        showBody("Initial redeemedState", redeemedState)
+
+
+
+
+
+
+
+
+
+
+
+
+         let redeemedState = await Wave.redeemed(s.Bob.address)
+        //showBody("Initial redeemedState", redeemedState)
 
         await Wave.connect(s.Bob).redeem()
         await mineBlock()
 
         redeemedState = await Wave.redeemed(s.Bob.address)
-        showBody("new redeemedState", redeemedState)
+        //showBody("new redeemedState", redeemedState)
 
         let isEnabled = await Wave.isEnabled()
-        showBody("isEnabled: ", isEnabled)
+        //showBody("isEnabled: ", isEnabled)
     
 
-        showBody("FAST FORWARD")
+        //showBody("FAST FORWARD")
         await fastForward(OneWeek)
         await mineBlock()
+         */
 
-       
-        
+
+
 
 
         //await expect(Wave.connect(s.Frank).redeem()).to.be.revertedWith("can't redeem yet")
