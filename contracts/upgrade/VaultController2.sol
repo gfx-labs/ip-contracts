edbe92f5b2ae274d33f05531b7ecae5068c90ceb
Jake B <jbarber@gfxlabs.io>
2022-05-11 14:46:24 -0500

    Explicit upgradeability tests done
    Former-commit-id: d0cc3fab6c9e2e3d6fc3ea496772f753ad12c4fa

---
+++ contracts/upgrade/VaultController2.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../lending/VaultController.sol";
+import "./IVaultController2.sol";
+
+contract VaultController2 is VaultController, IVaultController2 {
+  //CHANGED extend storage
+  uint256 public newThing;
+
+  //CHANGED new event
+  event ThingChanged(uint256 newThing);
+
+  //CHANGED new function
+  function changeTheThing(uint256 _newThing) public override {
+      newThing = _newThing;
+      emit ThingChanged(_newThing);
+  }
+
+
+}
