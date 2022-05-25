215795846c6b4165629fc3b14420aa1627d949d8
Jake B <jbarber@gfxlabs.io>
2022-05-24 19:00:57 -0500

    Ready to merge into cleanup
    Former-commit-id: 22b59d40118e27b02e027558cf311cac7a0672a4

--- contracts/lending/IVault.sol
+++ contracts/lending/IVault.sol
@@ -38,7 +38,7 @@ interface IVault is VaultEvents {
 
   function modifyLiability(bool increase, uint256 base_amount) external returns (uint256);
 
-  function depositETH() external payable;
+  //function depositETH() external payable;
 
-  function withdrawEther(uint256 amount) external;
+  //function withdrawEther(uint256 amount) external;
 }
--- test/isolated/index.ts
+++ test/isolated/index.ts
@@ -2,5 +2,4 @@ export * from "./noLoans/index"
 export * from "./noReserve/index"
 export * from "./uniPool/index"
 export * from "./upgrade/index"
-export * from "./naitiveEth/index"
 
