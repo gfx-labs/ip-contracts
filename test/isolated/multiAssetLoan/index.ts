4077e642d751a331ba2cd5c08a2ca0973e02f07f
Jake B <jbarber@gfxlabs.io>
2022-06-01 14:55:03 -0500

    cleanup

--- test/isolated/multiAssetLoan/index.ts
+++ test/isolated/multiAssetLoan/index.ts
@@ -1,3 +1,3 @@
 export * from "./1_setup"
 export * from "./2_deploy"
-export * from "./4_multiAssetLoan"
+export * from "./3_multiAssetLoan"
--- test/isolated/upgrade/index.ts
+++ test/isolated/upgrade/index.ts
@@ -1,3 +1,3 @@
 export * from "./1_setup"
 export * from "./2_deploy"
-export * from "./4_upgrade"
+export * from "./3_upgrade"
