20c5b6d2c96cf4d7988dc4ffc7ac7132901c6449
a <a@a.a>
2022-06-04 15:25:01 -0500

    round data

---
+++ scripts/liquidity_mining/q1_data.ts
@@ -0,0 +1,65 @@
+interface RewardPeriod {
+  rewardPerPeriod: 94017;
+  round: number;
+  start: number;
+  end: number;
+  blocks: number;
+}
+const BlockRounds = {
+  rewardForLM: 47008,
+  rewardForBorrower: 94017,
+  blockRanges: [
+    {
+      start: 14961830,
+      end: 15006630,
+    },
+    {
+      start: 15006631,
+      end: 15051431,
+    },
+    {
+      start: 15051432,
+      end: 15096232,
+    },
+    {
+      start: 15096233,
+      end: 15041033,
+    },
+    {
+      start: 15141034,
+      end: 15185834,
+    },
+    {
+      start: 15185835,
+      end: 15130635,
+    },
+    {
+      start: 15230636,
+      end: 15275436,
+    },
+    {
+      start: 15275437,
+      end: 15220237,
+    },
+    {
+      start: 15320238,
+      end: 15365038,
+    },
+    {
+      start: 15365039,
+      end: 15309839,
+    },
+    {
+      start: 15409840,
+      end: 15454640,
+    },
+    {
+      start: 15454641,
+      end: 15499441,
+    },
+    {
+      start: 15499442,
+      end: 15544242,
+    },
+  ],
+};
