bb4b29d2be6a32a2d114779236b6ba4dda36087e
Jake B <jbarber@gfxlabs.io>
2022-06-07 18:10:40 -0500

    Cleaned up V3 tests, merging to master

--- test/isolated/uniV3Pool/3_uniPool.ts
+++ test/isolated/uniV3Pool/3_uniPool.ts
@@ -146,56 +146,13 @@ describe("Test Uniswap V3 pool with rebasing USDi token", () => {
 
 
     /**
-     * try to make the exact WETH/USDI pool on v3 and steal the args from tenderly? 
-     * use the exact same amount of USDi vs wETH that I can get on polygon, and match the TX 1:1
-     * 
-     * wallet: 0x2243b90CCaF4a03F7289502722D8665E3d4f2972
-     * USDC: 0xbEed11d5c8c87FaCbf3f81728543eb8cB6CBa939
-     * USDi: 0x203c05ACb6FC02F5fA31bd7bE371E7B213e59Ff7
-     * wETH: 0x8afBfe06dA3D035c82C5bc55C82EB3FF05506a20
-     * 
-     * POOL PARAMS USDi - wETH - wETH price 1750 USDi - 0.0005714 wETH per USDI
-     * 1% fee tier 
-     * Min price: 1509.7 - 0.00050065 wETH per USDI
-     * max price: 1997.4 - 0.0006624 wETH per USDI
-     * 
-     * DEPOSIT AMOUNTS
-     * wETH: 5 - 4.999
-     * USDI: 9,745.435642333408348323
+     * Modeled after a real TX on polygon and traced in tenderly, TXs below
      * 
      * Approve USDI TX: 0x9774719f616dae952e35b9a31efa69ad2eb29ab89f7d345002860022ebcee739
      * https://polygonscan.com/tx/0x9774719f616dae952e35b9a31efa69ad2eb29ab89f7d345002860022ebcee739
      * 
      * TX ID: 0x19fdfff24c5dda5d2fcb032bda8cc4482270a8d17452a7478be427b595fe5408
      * https://polygonscan.com/tx/0x19fdfff24c5dda5d2fcb032bda8cc4482270a8d17452a7478be427b595fe5408
-     * 
-     * Pool Address: 0x12370F952775cd268aeaBbc36671342245C32965
-     * 
-     * 
-     * Tenderly data of note: 
-     * deploy() in createPool() - tickSpacing: 200
-     * 
-     * Initialize: sqrtPricex96: 1893862710253677737936450510
-     * 
-     * Mint Params: 
-     * "token0":"0x203c05acb6fc02f5fa31bd7be371e7b213e59ff7"
-        "token1":"0x8afbfe06da3d035c82c5bc55c82eb3ff05506a20"
-        "fee":"10000"
-        "tickLower":"-76000"
-        "tickUpper":"-73200"
-        "amount0Desired":"9745435642333408348323"
-        "amount1Desired":"5000000000000000000"
-        "amount0Min":"9404681434654713997304"
-        "amount1Min":"4804319378770569775"
-        "recipient":"0x2243b90ccaf4a03f7289502722d8665e3d4f2972"
-        "deadline":"1654278867"
-     * 
-        Output: 
-        "tokenId":"127253"
-        "liquidity":"3270355854394780560229"
-        "amount0":"9745435642333408348323"
-        "amount1":"5000000000000000000"
-
      */
     it("Use borrowed USDi to make a uni v3 pool", async () => {
 
@@ -277,12 +234,6 @@ describe("Test Uniswap V3 pool with rebasing USDi token", () => {
 
     /**
      * tenderly TX 0x527383fa57675b25e47bb1eacfbb691ffb5e8c95d53747f8b3f7f4549d2407e7
-     * amountIn: 100000000000000000000 //100 USDI
-     * amountOutMInimum: 56246150143478975 //0.056246150143478975 wETH
-     * sqrtPriceLimitX96: 0
-     * 
-     * USDi: 0x203c05ACb6FC02F5fA31bd7bE371E7B213e59Ff7
-     * wETH: 0x8afBfe06dA3D035c82C5bc55C82EB3FF05506a20
      */
     it("Dave does a small swap", async () => {
 
@@ -294,25 +245,15 @@ describe("Test Uniswap V3 pool with rebasing USDi token", () => {
         const startUSDIdave = await s.USDI.balanceOf(s.Dave.address)
         expect(await toNumber(startUSDIdave)).to.be.gt(100)//Dave has enough USDi
 
-        let balance = await s.USDI.balanceOf(poolV3.address)
-        showBody("pool USDI: ", await toNumber(balance))
-        balance = await s.WETH.balanceOf(poolV3.address)
-        showBody("pool wETH: ", await toNumber(balance))
-
         const daveWeth = await s.WETH.balanceOf(s.Dave.address)
         expect(daveWeth).to.eq(0)//dave has no weth
-
-        balance = await s.USDI.balanceOf(s.Dave.address)
-        showBody("Dave USDI: ", await toNumber(balance))
 
         //approve router for 100 USDi
         await s.USDI.connect(s.Dave).approve(router.address, swapUSDIamount)
         await mineBlock()
 
-
         const block = await currentBlock()
         const deadline = block.timestamp + 500
-
 
         const swapParams = [
             s.USDI.address.toString(), //tokenIn
@@ -329,7 +270,7 @@ describe("Test Uniswap V3 pool with rebasing USDi token", () => {
         await mineBlock()
 
         //dave received the correct amount of weth
-        balance = await s.WETH.balanceOf(s.Dave.address)
+        let balance = await s.WETH.balanceOf(s.Dave.address)
         expect(await toNumber(balance)).to.be.closeTo(0.05, 0.01)
     })
 
@@ -344,7 +285,6 @@ describe("Test Uniswap V3 pool with rebasing USDi token", () => {
 
         const poolUSDI = await s.USDI.balanceOf(poolV3.address)
         expect(await toNumber(poolUSDI)).to.be.gt(await toNumber(usdiAmount))//interest accrued while in the pool
-        showBodyCyan(await toNumber(poolUSDI))
     })
 
     it("Collect fee from pool, unclaimed USDi rewards do not accrue interest", async () => {
@@ -366,8 +306,6 @@ describe("Test Uniswap V3 pool with rebasing USDi token", () => {
 
         //100 USDI swapped, swap fee is 1%, Bob should profit 1 USDI from swap
         expect(await toNumber(difference)).to.eq(1)
-
-
     })
 
     it("remove all liquidity from pool and receive USDi + interest ", async () => {
@@ -402,10 +340,8 @@ describe("Test Uniswap V3 pool with rebasing USDi token", () => {
         //showBody(args)
         expect(args.tokenId.toNumber()).to.eq(tokenId)
 
-        
         await s.VaultController.calculateInterest()
         await mineBlock()
-
 
         const collectParams = [
             tokenId,        //tokenId
@@ -417,22 +353,20 @@ describe("Test Uniswap V3 pool with rebasing USDi token", () => {
         await NFPM.connect(s.Bob).collect(collectParams)
         await mineBlock()
 
-
         let balance = await s.USDI.balanceOf(s.Bob.address)
-        showBody("Starting Bob USDI: ", await toNumber(bobUSDI))
-        showBody("Ending Bob USDI  : ", await toNumber(balance))
+        let difference = balance.sub(bobUSDI)
+        expect(await toNumber(difference)).to.be.closeTo(await toNumber(usdiAmount), 100)//~correct amount of USDI removed from pool
 
         balance = await s.WETH.balanceOf(s.Bob.address)
-        showBody("Starting Bob weth: ", await toNumber(bobWETH))
-        showBody("Ending Bob weth  : ", await toNumber(balance))
+        expect(await toNumber(balance)).to.be.closeTo(await toNumber(wETHamount), 0.1)//~correct amount of WETH removed from pool
 
         balance = await s.USDI.balanceOf(poolV3.address)
-        showBody("Starting pool USDI: ", await toNumber(poolUSDi))
-        showBody("Ending pool USDI  : ", await toNumber(balance))
+        difference = poolUSDi.sub(balance)
+        expect(await toNumber(difference)).to.be.closeTo(await toNumber(usdiAmount), 100)//~correct amount of USDI removed from pool
+        expect(await toNumber(balance)).to.be.gt(await toNumber(usdiAmount))//Large amount of USDI stuck in pool
 
         balance = await s.WETH.balanceOf(poolV3.address)
-        showBody("Starting pool weth: ", await toNumber(poolWETH))
-        showBody("Ending pool weth  : ", await toNumber(balance))
+        expect(await toNumber(balance)).to.be.closeTo(0, 0.001)//WETH removed from pool
 
     })
 
@@ -445,6 +379,6 @@ describe("Test Uniswap V3 pool with rebasing USDi token", () => {
         expect(liquidity).to.eq(0)
     })
 
-    
+
 
 })
