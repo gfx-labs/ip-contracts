8658d597e011dacd19747d5cea12c8b640be015b
Jake <jbarber@gfxlabs.io>
2022-10-20 17:52:45 -0500

    Verify tests working

--- test/upgrade8/queueAndExecute/3_verify.ts
+++ test/upgrade8/queueAndExecute/3_verify.ts
@@ -97,6 +97,7 @@ describe("Testing CappedToken functions", () => {
     it("Deposit underlying", async () => {
         expect(await s.LDO.balanceOf(s.Bob.address)).to.eq(s.LDO_Amount, "Bob has the expected amount of LDO")
         expect(await s.DYDX.balanceOf(s.Bob.address)).to.eq(s.DYDX_Amount, "Bob has the expected amount of DYDX")
+        expect(await s.CRV.balanceOf(s.Bob.address)).to.eq(s.CRV_Amount, "Bob has the expected amount of CRV")
 
 
         let caBalance = await s.CappedLDO.balanceOf(s.BobVault.address)
@@ -104,6 +105,9 @@ describe("Testing CappedToken functions", () => {
 
         caBalance = await s.CappedDYDX.balanceOf(s.BobVault.address)
         expect(caBalance).to.eq(0, "Bob's vault holds 0 capped DYDX at the start")
+
+        caBalance = await s.CappedCRV.balanceOf(s.BobVault.address)
+        expect(caBalance).to.eq(0, "Bob's vault holds 0 capped CRV at the start")
 
         await s.LDO.connect(s.Bob).approve(s.CappedLDO.address, s.LDO_Amount)
         await s.CappedLDO.connect(s.Bob).deposit(s.LDO_Amount, s.BobVaultID)
@@ -113,14 +117,18 @@ describe("Testing CappedToken functions", () => {
         await s.CappedDYDX.connect(s.Bob).deposit(s.DYDX_Amount, s.BobVaultID)
         await mineBlock()
 
-        caBalance = await s.CappedLDO.balanceOf(s.Bob.address)
-        expect(caBalance).to.eq(0, "Bob holds 0 capped LDO after deposit")
+        await s.CRV.connect(s.Bob).approve(s.CappedCRV.address, s.CRV_Amount)
+        await s.CappedCRV.connect(s.Bob).deposit(s.CRV_Amount, s.BobVaultID)
+        await mineBlock()
 
         caBalance = await s.CappedLDO.balanceOf(s.BobVault.address)
         expect(caBalance).to.eq(s.LDO_Amount, "Bob's vault received the capped LDO tokens")
 
         caBalance = await s.CappedDYDX.balanceOf(s.BobVault.address)
         expect(caBalance).to.eq(s.DYDX_Amount, "Bob's vault received the capped DYDX tokens")
+
+        caBalance = await s.CappedCRV.balanceOf(s.BobVault.address)
+        expect(caBalance).to.eq(s.CRV_Amount, "Bob's vault received the capped CRV tokens")
     })
 
     it("Try to exceed the LDO cap", async () => {
@@ -158,18 +166,36 @@ describe("Testing CappedToken functions", () => {
         await mineBlock()
     })
 
+    it("Try to exceed the CRV cap", async () => {
+
+        await s.CappedCRV.connect(s.Frank).setCap(s.CRV_Amount)
+        await mineBlock()
+
+        const cap = await s.CappedCRV.getCap()
+        expect(cap).to.eq(s.CRV_Amount, "Cap is still correct")
+        expect(await s.CappedCRV.totalSupply()).to.eq(cap, "Cap reached")
+
+        await s.CRV.connect(s.Gus).approve(s.CappedCRV.address, 1)
+        await mineBlock()
+        expect(s.CappedCRV.connect(s.Gus).deposit(1, s.BobVaultID)).to.be.revertedWith("cap reached")
+
+        await s.CappedDYDX.connect(s.Frank).setCap(s.CRV_Cap)
+        await mineBlock()
+    })
+
     it("No vault", async () => {
         const amount = BN("5e18")
         const startBal = await s.LDO.balanceOf(s.Gus.address)
-        showBody("Got balance:", await toNumber(startBal))
         expect(startBal).to.eq(s.LDO_Amount, "Balance correct")
-        showBody("Balance Correct")
 
         //await s.LDO.connect(s.Gus).approve(s.CappedLDO.address, amount) // LDO is not ERC20 - no approval
         //showBody("Approved")
         expect(s.CappedLDO.connect(s.Gus).deposit(amount, 99999)).to.be.revertedWith("invalid vault")
+        expect(s.CappedDYDX.connect(s.Gus).deposit(amount, 99999)).to.be.revertedWith("invalid vault")
+        expect(s.CappedCRV.connect(s.Gus).deposit(amount, 99999)).to.be.revertedWith("invalid vault")
+
     })
- 
+
     it("Deposit BAL with no voting vault", async () => {
         const amount = BN("5e18")
 
@@ -189,7 +215,7 @@ describe("Testing CappedToken functions", () => {
         //await s.LDO.connect(s.Gus).approve(s.CappedLDO.address, amount)
         expect(s.CappedLDO.connect(s.Gus).deposit(amount, gusVaultId)).to.be.revertedWith("invalid voting vault")
     })
-   
+
     it("Deposit DYDX with no voting vault", async () => {
         const amount = BN("5e18")
 
@@ -210,7 +236,26 @@ describe("Testing CappedToken functions", () => {
         expect(s.CappedDYDX.connect(s.Gus).deposit(amount, gusVaultId)).to.be.revertedWith("invalid voting vault")
     })
 
-    it("Eronious transfer of BAL and then withdraw with no voting vault", async () => {
+    it("Deposit CRV with no voting vault", async () => {
+        const amount = BN("5e18")
+
+        const startBal = await s.CRV.balanceOf(s.Gus.address)
+        expect(startBal).to.eq(s.CRV_Amount, "Balance correct")
+
+
+        //mint regular vault for gus
+        await expect(s.VaultController.connect(s.Gus).mintVault()).to.not
+            .reverted;
+        await mineBlock();
+        gusVaultId = await s.VaultController.vaultsMinted()
+        let vaultAddress = await s.VaultController.vaultAddress(gusVaultId)
+        gusVault = IVault__factory.connect(vaultAddress, s.Gus);
+        expect(await gusVault.minter()).to.eq(s.Gus.address);
+
+        expect(s.CappedCRV.connect(s.Gus).deposit(amount, gusVaultId)).to.be.revertedWith("invalid voting vault")
+    })
+
+    it("Eronious transfer of LDO and then withdraw with no voting vault", async () => {
         //transfer some underlying to cap contract instead of deposit?
         const transferAmount = BN("5e18")
         let balance = await s.LDO.balanceOf(s.Gus.address)
@@ -235,6 +280,19 @@ describe("Testing CappedToken functions", () => {
 
     })
 
+    it("Eronious transfer of CRV and then withdraw with no voting vault", async () => {
+        //transfer some underlying to cap contract instead of deposit?
+        const transferAmount = BN("5e18")
+        let balance = await s.CRV.balanceOf(s.Gus.address)
+        expect(balance).to.eq(s.CRV_Amount, "Starting CRV amount correct")
+        await s.LDO.connect(s.Gus).transfer(s.CappedCRV.address, transferAmount)
+        await mineBlock()
+
+        //try to withdraw - no voting vault
+        expect(gusVault.connect(s.Gus).withdrawErc20(s.CappedCRV.address, transferAmount)).to.be.revertedWith("only vaults")
+
+    })
+
     it("Try to withdraw eronious transfer after minting a voting vault", async () => {
         const transferAmount = BN("5e18")
 
@@ -253,44 +311,63 @@ describe("Testing CappedToken functions", () => {
 
         expect(gusVault.connect(s.Gus).withdrawErc20(s.CappedDYDX.address, transferAmount)).to.be.revertedWith("ERC20: burn amount exceeds balance")
     })
+    it("Try to withdraw eronious transfer after minting a voting vault", async () => {
+        const transferAmount = BN("5e18")
+
+        //mint a voting vault
+        await s.VotingVaultController.connect(s.Gus).mintVault(gusVaultId)
+        await mineBlock()
+
+        expect(gusVault.connect(s.Gus).withdrawErc20(s.CappedCRV.address, transferAmount)).to.be.revertedWith("ERC20: burn amount exceeds balance")
+    })
 
     /**
+       
        it("Try to withdraw more capped LDO than is possible given some cap tokens", async () => {
-
-        await s.CappedLDO.connect(s.Frank).setCap(BN("51e24"))
-        await mineBlock()
-        showBody("abouda")
-      
- 
-        await s.LDO.connect(s.Gus).approve(s.CappedLDO.address, BN("1e18"))
-        await mineBlock()
-        showBody("Approved")
   
-
-        await s.CappedLDO.connect(s.Gus).deposit(BN("1e18"), gusVaultId)
-        await mineBlock()
-        showBody("deposited")
-
-
-        let balance = await s.CappedLDO.balanceOf(gusVault.address)
-        expect(balance).to.eq(BN("1e18"), "Balance is correct")
-
-        showBody("trying")
-
-
-        expect(gusVault.connect(s.Gus).withdrawErc20(s.CappedLDO.address, BN("5e18"))).to.be.revertedWith("only cap token")
-
-        //Withdraw the amount that was deposited
-        await gusVault.connect(s.Gus).withdrawErc20(s.CappedLDO.address, BN("1e18"))
-        await mineBlock()
-        showBody("Withdrawn")
-
-        //return cap to expected amount
-        await s.CappedLDO.connect(s.Frank).setCap(s.LDO_Cap)
-        await mineBlock()
-        showBody("returned")
-
-    })
+          await s.CappedLDO.connect(s.Frank).setCap(BN("51e24"))
+          await mineBlock()
+          showBody("abouda")
+          let b = await s.LDO.balanceOf(s.Gus.address)
+          showBody("Bal: ", await toNumber(b))
+  
+          const allowance = await s.LDO.allowance(s.Gus.address, s.CappedLDO.address)
+  
+          showBody("Allowance: ", await toNumber(allowance))
+          showBody("Raw Alowa: ", allowance)
+  
+          showBody("Dif: ", await toNumber(b.sub(allowance)))
+  
+          await s.LDO.connect(s.Gus).approve(s.CappedLDO.address, b.sub(allowance))
+          await mineBlock()
+          showBody("Approved")
+  
+  
+          await s.CappedLDO.connect(s.Gus).deposit(BN("1e18"), gusVaultId)
+          await mineBlock()
+          showBody("deposited")
+  
+  
+          let balance = await s.CappedLDO.balanceOf(gusVault.address)
+          expect(balance).to.eq(BN("1e18"), "Balance is correct")
+  
+          showBody("trying")
+  
+  
+          expect(gusVault.connect(s.Gus).withdrawErc20(s.CappedLDO.address, BN("5e18"))).to.be.revertedWith("only cap token")
+  
+          //Withdraw the amount that was deposited
+          await gusVault.connect(s.Gus).withdrawErc20(s.CappedLDO.address, BN("1e18"))
+          await mineBlock()
+          showBody("Withdrawn")
+  
+          //return cap to expected amount
+          await s.CappedLDO.connect(s.Frank).setCap(s.LDO_Cap)
+          await mineBlock()
+          showBody("returned")
+  
+      })
+       
      */
 
     it("Try to withdraw more capped DYDX than is possible given some cap tokens", async () => {
@@ -316,11 +393,49 @@ describe("Testing CappedToken functions", () => {
         await mineBlock()
 
     })
+    it("Try to withdraw more capped CRV than is possible given some cap tokens", async () => {
+
+        await expect(s.VaultController.connect(s.Andy).mintVault()).to.not
+            .reverted;
+        await mineBlock();
+        let andyVaultId = await s.VaultController.vaultsMinted()
+        let vaultAddress = await s.VaultController.vaultAddress(andyVaultId)
+        let andyVault = IVault__factory.connect(vaultAddress, s.Andy);
+        expect(await andyVault.minter()).to.eq(s.Andy.address);
+
+        await s.VotingVaultController.connect(s.Andy).mintVault(andyVaultId)
+        await mineBlock()
+
+        await s.CappedCRV.connect(s.Frank).setCap(BN("51e24"))
+        await mineBlock()
+
+        await s.CRV.connect(s.Andy).approve(s.CappedCRV.address, BN("1e18"))
+        await s.CappedCRV.connect(s.Andy).deposit(BN("1e18"), andyVaultId)
+        await mineBlock()
+
+        let balance = await s.CappedCRV.balanceOf(andyVault.address)
+        expect(balance).to.eq(BN("1e18"), "Balance is correct")
+
+        expect(andyVault.connect(s.Andy).withdrawErc20(s.CappedCRV.address, BN("5e18"))).to.be.revertedWith("only cap token")
+
+        //Withdraw the amount that was deposited
+        await andyVault.connect(s.Andy).withdrawErc20(s.CappedCRV.address, BN("1e18"))
+        await mineBlock()
+
+
+        //return cap to expected amount
+        await s.CappedCRV.connect(s.Frank).setCap(s.CRV_Cap)
+        await mineBlock()
+
+    })
 
     it("Try to withdraw from a vault that is not yours", async () => {
         const amount = BN("250e18")
 
         await expect(s.BobVault.connect(s.Carol).withdrawErc20(s.CappedLDO.address, amount)).to.be.revertedWith("sender not minter")
+        await expect(s.BobVault.connect(s.Carol).withdrawErc20(s.CappedDYDX.address, amount)).to.be.revertedWith("sender not minter")
+        await expect(s.BobVault.connect(s.Carol).withdrawErc20(s.CappedCRV.address, amount)).to.be.revertedWith("sender not minter")
+
     })
 
 
@@ -371,5 +486,33 @@ describe("Testing CappedToken functions", () => {
         await s.CappedDYDX.connect(s.Bob).deposit(amount, s.BobVaultID)
         await mineBlock()
     })
+
+
+
+    it("Withdraw Underlying CRV", async () => {
+
+        const amount = BN("250e18")
+
+        const startCRV = await s.CRV.balanceOf(s.Bob.address)
+        const startCapCRV = await s.CappedCRV.balanceOf(s.BobVault.address)
+
+        expect(startCapCRV).to.be.gt(amount, "Enough capped CRV")
+
+        await s.BobVault.connect(s.Bob).withdrawErc20(s.CappedCRV.address, amount).catch(console.log)
+        await mineBlock()
+
+        let balance = await s.CRV.balanceOf(s.Bob.address).catch(console.log)
+        expect(balance).to.eq(startCRV.add(amount), "CRV balance changed as expected")
+
+        balance = await s.CappedCRV.balanceOf(s.BobVault.address).catch(console.log)
+        expect(balance).to.eq(startCapCRV.sub(amount), "CappedCRV balance changed as expected")
+
+        //Deposit again to reset for further tests
+        await s.CRV.connect(s.Bob).approve(s.CappedCRV.address, amount)
+        await s.CappedCRV.connect(s.Bob).deposit(amount, s.BobVaultID)
+        await mineBlock()
+    })
+
+
 
 })
