0f101bf611fddfabbb6d531253ab9761a6db10ef
Jake <jbarber@gfxlabs.io>
2023-01-25 16:59:19 -0600

    mainnet deploys are working

--- scripts/proposals/ZRX/deploy.ts
+++ scripts/proposals/ZRX/deploy.ts
@@ -20,7 +20,8 @@ import {
     ChainlinkTokenOracleRelay,
     ChainlinkTokenOracleRelay__factory,
     BalancerPeggedAssetRelay,
-    BalancerPeggedAssetRelay__factory
+    BalancerPeggedAssetRelay__factory,
+    UniswapV3OracleRelay__factory
 } from "../../../typechain-types";
 import { DeployContractWithProxy, DeployContract } from "../../../util/deploy";
 import { ProposalContext } from "../suite/proposal";
@@ -33,78 +34,44 @@ import { BigNumber } from "ethers";
 const { ethers, network, upgrades } = require("hardhat");
 const govAddress = "0x266d1020A84B9E8B0ed320831838152075F8C4cA"
 
+const ZRX_ADDR = "0xE41d2489571d322189246DaFA5ebDe1F4699F498"
+const ZRX_CAP = BN("1000000e18")
 
+const chainLinkZRX = "0x2da4983a622a8498bb1a21fae9d8f6c664939962"
+const ZRXuniPool = "0x14424eEeCbfF345B38187d0B8b749E56FAA68539"
+let CappedZRX: CappedGovToken
 
-const rEthPegProvider = "0x1a8F81c256aee9C640e14bB0453ce247ea0DFE6F"
-const rEthBalancerPool = "0x1E19CF2D73a72Ef1332C882F20534B6519Be0276"
-const rEthUniPool = "0xa4e0faA58465A2D369aa21B3e42d43374c6F9613"
-const rETHaddress = "0xae78736Cd615f374D3085123A210448E74Fc6393"
-
-const chainlinkCBETHfeed = "0x67eF3CAF8BeB93149F48e8d20920BEC9b4320510"
-const CBETH_POOL = "0x840DEEef2f115Cf50DA625F7368C24af6fE74410"
-const cbEthAddress = "0xBe9895146f7AF43049ca1c1AE358B0541Ea49704"
-
-const rEth_CAP = BN("3000e18")
-const cbETH_CAP = BN("4200e18")
-
-
-let anchorCBETH: UniswapV3TokenOracleRelay
-let mainCBETH: ChainlinkOracleRelay
-let anchorViewCBETH: AnchoredViewRelay
-
-let anchorRETH: BalancerPeggedAssetRelay
-let mainRETH: UniswapV3TokenOracleRelay
-let anchorViewRETH: AnchoredViewRelay
-
-let CappedCBETH: CappedGovToken
-let CappedRETH: CappedGovToken
+let anchorZRX: UniswapV3TokenOracleRelay
+let mainZRX: ChainlinkOracleRelay
+let anchorViewZRX: AnchoredViewRelay
 
 const deployCapTokens = async (deployer: SignerWithAddress) => {
     const proxy = ProxyAdmin__factory.connect(d.ProxyAdmin, deployer)
-    const CappedGovFactory = await ethers.getContractFactory("CappedGovToken")
+    //const CappedGovFactory = await ethers.getContractFactory("CappedGovToken")
 
-    //deploy capped rETH
-    const ucrETH = await CappedGovFactory.deploy()
-    
-    const crETH = await new TransparentUpgradeableProxy__factory(deployer).deploy(
-        ucrETH.address,
+    //deploy capped ZRX
+    const ucZRX = await new CappedGovToken__factory(deployer).deploy()
+
+    const cZRX = await new TransparentUpgradeableProxy__factory(deployer).deploy(
+        ucZRX.address,
         proxy.address,
         "0x"
     )
-    await crETH.deployed()
+    await cZRX.deployed()
 
-    CappedRETH = CappedGovFactory.attach(crETH.address)
-    console.log("Capped rETH deployed to: ", crETH.address)
-    const initrETH = await CappedRETH.initialize(
-        "Capped rETH",
-        "crETH",
-        rETHaddress,
+    CappedZRX = new CappedGovToken__factory(deployer).attach(cZRX.address)
+    console.log("Capped ZRX deployed to: ", cZRX.address)
+    const initZRX = await CappedZRX.initialize(
+        "Capped ZRX",
+        "cZRX",
+        ZRX_ADDR,
         d.VaultController,
         d.VotingVaultController
     )
-    await initrETH.wait()
-    console.log("Capped rETH Initialized", CappedRETH.address)
+    await initZRX.wait()
+    console.log("Capped ZRX Initialized", CappedZRX.address)
 
-    //deploy capped cbETH
-    const uccbETH = await CappedGovFactory.deploy()
-    const ccbETH = await new TransparentUpgradeableProxy__factory(deployer).deploy(
-        uccbETH.address,
-        proxy.address,
-        "0x"
-    )
-    await ccbETH.deployed()
 
-    CappedCBETH = CappedGovFactory.attach(ccbETH.address)
-    console.log("Capped cbETH deployed to: ", ccbETH.address)
-    const initcbETH = await CappedCBETH.initialize(
-        "Capped cbETH",
-        "ccbETH",
-        cbEthAddress,
-        d.VaultController,
-        d.VotingVaultController
-    )
-    await initcbETH.wait()
-    console.log("Capped cbETH Initialized", CappedCBETH.address)
 
 }
 
@@ -112,82 +79,54 @@ const deployOracles = async (deployer: SignerWithAddress) => {
     let peggedBalancerFactory = new BalancerPeggedAssetRelay__factory(deployer)
     let UniV3Factory = new UniswapV3TokenOracleRelay__factory(deployer)
     let chainlinkFactory = new ChainlinkOracleRelay__factory(deployer)
-    //let mainTokenFactory = new ChainlinkTokenOracleRelay__factory(deployer)
+    let mainTokenFactory = new ChainlinkTokenOracleRelay__factory(deployer)
     let anchorViewFactory = new AnchoredViewRelay__factory(deployer)
 
 
 
-    anchorRETH = await peggedBalancerFactory.deploy(
+    anchorZRX = await UniV3Factory.deploy(
         14400,
-        rEthBalancerPool,
-        rEthPegProvider,
+        ZRXuniPool,
+        true,
         BN("1"),
         BN("1")
     )
-    await anchorRETH.deployed()
-    console.log("rETH Balancer Pegged anchor deployed: ", anchorRETH.address)
-    showBodyCyan("rETH Balancer Pegged relay price: ", await toNumber(await anchorRETH.currentValue()))
+    await anchorZRX.deployed()
+    console.log("ZRX anchor deployed: ", anchorZRX.address)
+    showBodyCyan("ZRX anchor relay price: ", await toNumber(await anchorZRX.currentValue()))
 
-    mainRETH = await UniV3Factory.deploy(
-        14400,
-        rEthUniPool,
-        false,
+    mainZRX = await mainTokenFactory.deploy(
+        chainLinkZRX,
         BN("1"),
-        BN('1')
-    )
-    await mainRETH.deployed()
-    console.log("rETH Uniswap V3 anchor deployed: ", mainRETH.address)
-    showBodyCyan("rETH Uni V3 relay price: ", await toNumber(await mainRETH.currentValue()))
-
-
-    anchorViewRETH = await anchorViewFactory.deploy(
-        anchorRETH.address, //Balancer pegged
-        mainRETH.address, //Uni V3
-        BN("10"),
-        BN("100")
-    )
-
-    await anchorViewRETH.deployed()
-    console.log("rETH anchor view deployed: ", anchorViewRETH.address)
-    showBodyCyan("rETH anchor view price: ", await toNumber(await anchorViewRETH.currentValue()))
-
-    anchorCBETH = await UniV3Factory.deploy(
-        14400,
-        CBETH_POOL,
-        false,
-        BN("1"),
-        BN('1')
-    )
-    await anchorCBETH.deployed()
-
-    console.log("cbETH Uniswap V3 anchor deployed: ", anchorCBETH.address)
-    showBodyCyan("cbETH anchor relay price: ", await toNumber(await anchorCBETH.currentValue()))
-
-
-    mainCBETH = await chainlinkFactory.deploy(
-        chainlinkCBETHfeed,
-        BN("1e10"),
         BN("1")
     )
-    await mainCBETH.deployed()
-    console.log("cbETH chainlink data relay deployed: ", mainCBETH.address)
-    showBodyCyan("cbETH chainlink data feed price: ", await toNumber(await mainCBETH.currentValue()))
+    await mainZRX.deployed()
+    console.log("ZRX Main relay deployed: ", mainZRX.address)
+    showBodyCyan("ZRX Main relay price: ", await toNumber(await mainZRX.currentValue()))
 
-    anchorViewCBETH = await anchorViewFactory.deploy(
-        anchorCBETH.address,
-        mainCBETH.address,
-        BN("10"),
+
+    anchorViewZRX = await anchorViewFactory.deploy(
+        anchorZRX.address, //Balancer pegged
+        mainZRX.address, //Uni V3
+        BN("20"),
         BN("100")
     )
-    await anchorViewCBETH.deployed()
-    console.log("Anchor View cbETH deployed: ", anchorViewCBETH.address)
-    showBodyCyan("cbETH Anchor view price: ", await toNumber(await anchorViewCBETH.currentValue()))
+
+    await anchorViewZRX.deployed()
+    console.log("ZRX anchor view deployed: ", anchorViewZRX.address)
+    showBodyCyan("ZRX anchor view price: ", await toNumber(await anchorViewZRX.currentValue()))
+
+
 
 
 }
 
 const deploy = async (deployer: SignerWithAddress) => {
 
+    //deploy new implementation
+    const implementation = await new VaultController__factory(deployer).deploy()
+
+    console.log("New VC implementation deployed to: ", implementation.address)
 
     await deployCapTokens(deployer)
 
@@ -197,18 +136,17 @@ const deploy = async (deployer: SignerWithAddress) => {
 
     console.log("All oracles have been deployed successfully")
 
-    await CappedRETH.setCap(rEth_CAP)
-    console.log("Set rETH cap to: ", await toNumber(rEth_CAP))
+    await CappedZRX.setCap(ZRX_CAP)
+    console.log("Set ZRX cap to: ", await toNumber(ZRX_CAP))
 
-    await CappedCBETH.setCap(cbETH_CAP)
-    console.log("Set cbETH cap to: ", await toNumber(cbETH_CAP))
+
 
 };
 
 async function main() {
     //enable this for testing on hardhat network, disable for testnet/mainnet deploy
-    await network.provider.send("evm_setAutomine", [true])
-    await reset(15999131)
+    //await network.provider.send("evm_setAutomine", [true])
+    //await reset(16486122)
 
 
     const accounts = await ethers.getSigners();
@@ -224,3 +162,18 @@ main()
         console.error(error);
         process.exit(1);
     });
+/**
+ * OUTPUT 
+New VC implementation deployed to:  0x9BDb5575E24EEb2DCA7Ba6CE367d609Bdeb38246
+Capped ZRX deployed to:  0xDf623240ec300fD9e2B7780B34dC2F417c0Ab6D2
+Capped ZRX Initialized 0xDf623240ec300fD9e2B7780B34dC2F417c0Ab6D2
+All Cap Tokens deployed
+ZRX anchor deployed:  0xCfae22EAD912F7F8299113915bEC0c92F98Cd4a7
+    ↓   ZRX anchor relay price:  0.219269449809822
+ZRX Main relay deployed:  0x8cd06C41617B0882A2a5D1334BdE48664fD89b5A
+    ↓   ZRX Main relay price:  0.2203965700776926
+ZRX anchor view deployed:  0xEF12fa3183362506A2dd0ff1CF06b2f4156e751E
+    ↓   ZRX anchor view price:  0.2203965700776926
+All oracles have been deployed successfully
+Set ZRX cap to:  1000000
+ */
---
+++ test/upgrades/uniLTV_ZRX/mainnetDeployments/2_queueAndExecute.ts
@@ -0,0 +1,251 @@
+import { s } from "../scope";
+import { upgrades, ethers } from "hardhat";
+import { expect, assert } from "chai";
+import { showBody, showBodyCyan } from "../../../../util/format";
+import { impersonateAccount, ceaseImpersonation } from "../../../../util/impersonator"
+import * as fs from 'fs';
+
+import { BN } from "../../../../util/number";
+import {
+  IVault__factory,
+  GovernorCharlieDelegate,
+  GovernorCharlieDelegate__factory,
+  CappedGovToken__factory,
+  UniswapV3TokenOracleRelay__factory,
+  UniswapV3TokenOracleRelay,
+  AnchoredViewRelay,
+  AnchoredViewRelay__factory,
+  OracleMaster__factory,
+  VaultController__factory,
+  VotingVaultController__factory,
+  ChainlinkOracleRelay,
+  ChainlinkOracleRelay__factory,
+  ChainlinkTokenOracleRelay__factory,
+  GeneralizedBalancerOracle,
+  GeneralizedBalancerOracle__factory,
+  OracleRETH,
+  BalancerPeggedAssetRelay,
+  UniswapV2OracleRelay__factory,
+  VaultController,
+  ProxyAdmin__factory
+} from "../../../../typechain-types";
+import {
+  advanceBlockHeight,
+  fastForward,
+  mineBlock,
+  OneWeek,
+  OneYear,
+  currentBlock
+} from "../../../../util/block";
+import { toNumber } from "../../../../util/math";
+import { ProposalContext } from "../../../../scripts/proposals/suite/proposal";
+import { DeployContractWithProxy, DeployContract } from "../../../../util/deploy";
+import { OracleRETH__factory } from "../../../../typechain-types/factories/oracle/External/OracleRETH.sol/OracleRETH__factory";
+import { BalancerPeggedAssetRelay__factory } from "../../../../typechain-types/factories/oracle/External/BalancerPeggedAssetRelay.sol";
+
+let anchorZRX: UniswapV3TokenOracleRelay
+let mainZRX: ChainlinkOracleRelay
+let anchorViewZRX: AnchoredViewRelay
+
+
+require("chai").should();
+describe("Verify Contracts", () => {
+  it("Should return the right name, symbol, and decimals", async () => {
+
+    expect(await s.USDI.name()).to.equal("USDI Token");
+    expect(await s.USDI.symbol()).to.equal("USDI");
+    expect(await s.USDI.decimals()).to.equal(18);
+    //expect(await s.USDI.owner()).to.equal(s.Frank.address);
+    //s.owner = await s.USDI.owner()
+    s.pauser = await s.USDI.pauser()
+  });
+
+
+  it("Check data on VaultControler", async () => {
+    let tokensRegistered = await s.VaultController.tokensRegistered()
+    expect(tokensRegistered).to.be.gt(0)
+    let interestFactor = await s.VaultController.interestFactor()
+    expect(await toNumber(interestFactor)).to.be.gt(1)
+
+  });
+
+  it("mint vaults for testing", async () => {
+    //showBody("bob mint vault")
+    await expect(s.VaultController.connect(s.Bob).mintVault()).to.not
+      .reverted;
+    await mineBlock();
+    s.BobVaultID = await s.VaultController.vaultsMinted()
+    let vaultAddress = await s.VaultController.vaultAddress(s.BobVaultID)
+    s.BobVault = IVault__factory.connect(vaultAddress, s.Bob);
+    expect(await s.BobVault.minter()).to.eq(s.Bob.address);
+
+    //showBody("carol mint vault")
+    await expect(s.VaultController.connect(s.Carol).mintVault()).to.not
+      .reverted;
+    await mineBlock();
+    s.CaroLVaultID = await s.VaultController.vaultsMinted()
+    vaultAddress = await s.VaultController.vaultAddress(s.CaroLVaultID)
+    s.CarolVault = IVault__factory.connect(vaultAddress, s.Carol);
+    expect(await s.CarolVault.minter()).to.eq(s.Carol.address);
+  });
+});
+
+
+
+describe("Setup, Queue, and Execute proposal", () => {
+  const governorAddress = "0x266d1020A84B9E8B0ed320831838152075F8C4cA";
+  const proposer = "0x958892b4a0512b28AaAC890FC938868BBD42f064"//0xa6e8772af29b29b9202a073f8e36f447689beef6 ";
+  const prop = ethers.provider.getSigner(proposer)
+
+  let gov: GovernorCharlieDelegate;
+
+  let proposal: number
+
+  let out: any
+
+  const implementation = "0x9BDb5575E24EEb2DCA7Ba6CE367d609Bdeb38246"
+  const CappedZRX_ADDR = "0xDf623240ec300fD9e2B7780B34dC2F417c0Ab6D2"
+  const anchorViewAddr = "0xEF12fa3183362506A2dd0ff1CF06b2f4156e751E"
+
+  it("Connect to mainnet deployments", async () => {
+
+    s.CappedZRX = new CappedGovToken__factory(s.Frank).attach(CappedZRX_ADDR)
+    anchorViewZRX = new AnchoredViewRelay__factory(s.Frank).attach(anchorViewAddr)
+
+  })
+
+  it("Transfer ownership for future tests", async () => {
+    await impersonateAccount(s.deployer._address)
+    await s.CappedZRX.connect(s.deployer).transferOwnership(s.owner._address)
+    await ceaseImpersonation(s.deployer._address)
+  })
+
+  it("Makes the new proposal", async () => {
+
+    await impersonateAccount(proposer)
+    gov = new GovernorCharlieDelegate__factory(prop).attach(
+      governorAddress
+    );
+
+    const proposal = new ProposalContext("ZRX&UNI_LTV")
+
+    const addOracleZRX = await new OracleMaster__factory(prop).
+      attach(s.Oracle.address).
+      populateTransaction.setRelay(
+        s.CappedZRX.address,
+        anchorViewZRX.address
+      )
+
+    const listZRX = await new VaultController__factory(prop).
+      attach(s.VaultController.address).
+      populateTransaction.registerErc20(
+        s.CappedZRX.address,
+        s.ZRX_LTV,
+        s.CappedZRX.address,
+        s.ZRX_LiqInc
+      )
+
+    const registerZRX_VVC = await new VotingVaultController__factory(prop).
+      attach(s.VotingVaultController.address).
+      populateTransaction.registerUnderlying(
+        s.ZRX.address,
+        s.CappedZRX.address
+      )
+
+
+    const OracleVerbose = OracleMaster__factory.connect(s.Oracle.address, s.Frank)
+    const VaultControllerVerbose = VaultController__factory.connect(s.VaultController.address, s.Frank)
+    const currentOracle = await OracleVerbose._relays(s.UNI.address)
+    const currentLiqInc = await VaultControllerVerbose._tokenAddress_liquidationIncentive(s.UNI.address)
+
+    //showBody("Current Oracle: ", currentOracle)
+
+    expect(await toNumber(currentLiqInc)).to.eq(0.15, "Current Liquidation Incentive is 0.15, no change needed")
+
+    const updateUniLTV = await new VaultController__factory(prop).
+      attach(s.VaultController.address).
+      populateTransaction.updateRegisteredErc20(
+        s.UNI.address,
+        s.NEW_UNI_LTV,
+        s.UNI.address,
+        currentLiqInc
+      )
+
+    const upgradeVC = await new ProxyAdmin__factory(prop).
+      attach(s.ProxyAdmin.address).
+      populateTransaction.upgrade(
+        s.VaultController.address,
+        implementation
+      )
+
+    //upgrade VC
+    proposal.addStep(upgradeVC, "upgrade(address,address)")
+    //UNI LTV
+    proposal.addStep(updateUniLTV, "updateRegisteredErc20(address,uint256,address,uint256)")
+
+
+
+    //list ZRX
+    proposal.addStep(addOracleZRX, "setRelay(address,address)")
+    proposal.addStep(listZRX, "registerErc20(address,uint256,address,uint256)")
+    proposal.addStep(registerZRX_VVC, "registerUnderlying(address,address)")
+
+
+
+
+    await ceaseImpersonation(proposer)
+
+    out = proposal.populateProposal()
+    //showBody(out)
+
+
+  })
+
+
+
+
+  it("queue and execute", async () => {
+    const votingPeriod = await gov.votingPeriod()
+    const votingDelay = await gov.votingDelay()
+    const timelock = await gov.proposalTimelockDelay()
+
+    const block = await currentBlock()
+    const votes = await s.IPT.getPriorVotes(proposer, block.number - 2)
+    expect(await toNumber(votes)).to.eq(45000000, "Correct number of votes delegated")
+
+    await impersonateAccount(proposer)
+    await gov.connect(prop).propose(
+      out.targets,
+      out.values,
+      out.signatures,
+      out.calldatas,
+      "List ZRX & update UNI LTV",
+      false
+    )
+    await mineBlock()
+    proposal = Number(await gov.proposalCount())
+    showBodyCyan("Advancing a lot of blocks...")
+    await advanceBlockHeight(votingDelay.toNumber());
+
+    await gov.connect(prop).castVote(proposal, 1)
+    await mineBlock()
+
+    showBodyCyan("Advancing a lot of blocks again...")
+    await advanceBlockHeight(votingPeriod.toNumber());
+    await mineBlock()
+
+    await gov.connect(prop).queue(proposal);
+    await mineBlock()
+
+    await fastForward(timelock.toNumber());
+    await mineBlock()
+
+    await gov.connect(prop).execute(proposal);
+    await mineBlock();
+
+
+    await ceaseImpersonation(proposer)
+
+  })
+
+})
