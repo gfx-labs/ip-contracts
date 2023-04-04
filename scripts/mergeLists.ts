6810bb144ae43077049a9427ac39623e41b9dd57
Jake Barber <jbarber@gfxlabs.io>
2023-04-04 01:13:07 -0500

    week 39

--- scripts/mergeLists.ts
+++ scripts/mergeLists.ts
@@ -30,8 +30,8 @@ import { json } from "stream/consumers";
 
 const { ethers, network, upgrades } = require("hardhat");
 
-const LPS = require('../rewardtree/lenders_16798668-16849068')
-const BORROWERS = require('../rewardtree/borrowers_16798668-16849068')
+const LPS = require('../rewardtree/lenders_16849069-16899469')
+const BORROWERS = require('../rewardtree/borrowers_16849069-16899469')
 
 
 //for format minter:amount  ===>> {minter: minter, amount: amount}
@@ -95,8 +95,8 @@ async function main() {
         formatObject[object.minter] = object.amount
     }
 
-    writeFileSync(`rewardtree/mergedAndFormatWeek38.json`, JSON.stringify(valueAdjusted), 'utf8')
-    writeFileSync(`rewardtree/week38Object.json`, JSON.stringify(formatObject), 'utf8')
+    writeFileSync(`rewardtree/mergedAndFormatWeek39.json`, JSON.stringify(valueAdjusted), 'utf8')
+    writeFileSync(`rewardtree/week39Object.json`, JSON.stringify(formatObject), 'utf8')
 
     console.log("DONE")
 
