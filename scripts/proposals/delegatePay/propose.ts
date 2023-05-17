874d5cd205615c1cf92017639e3bc304fbbfe47c
Jake <jbarber@gfxlabs.io>
2023-05-17 17:46:23 -0500

    proposal

--- scripts/proposals/delegatePay/propose.ts
+++ scripts/proposals/delegatePay/propose.ts
@@ -9,10 +9,16 @@ import {
 } from "../../../typechain-types";
 import { ProposalContext } from "../suite/proposal";
 import { d } from "../DeploymentInfo";
-import { showBody } from "../../../util/format";
+import { showBody, showBodyCyan } from "../../../util/format";
+import { getGas, toNumber } from "../../../util/math";
+
+import { impersonateAccount, ceaseImpersonation } from "../../../util/impersonator"
+
 import * as fs from 'fs';
-import { resetCurrent } from "../../../util/block";
+import { fastForward, hardhat_mine, mineBlock, resetCurrent } from "../../../util/block";
 import hre from 'hardhat'
+import { Signer } from "ethers";
+import { start } from "repl";
 const { ethers, network } = require("hardhat");
 
 const govAddress = "0x266d1020A84B9E8B0ed320831838152075F8C4cA"
@@ -32,10 +38,8 @@ const propose = async (proposer: SignerWithAddress) => {
             amount
         )
 
-
-
     //list TOKEN
-    proposal.addStep(sendUSDI, "setRelay(address,address)")
+    proposal.addStep(sendUSDI, "transfer(address,uint256)")
 
 
     let out = proposal.populateProposal()
@@ -55,8 +59,12 @@ const propose = async (proposer: SignerWithAddress) => {
         proposalText,
         false
     )
+    await testProposal(proposal.populateProposal())
+    //wait testImpersonation(proposer)
 
-    if (proposeFromScript) {
+
+    
+     if (proposeFromScript) {
         console.log("Sending proposal from ", proposer.address)
         const result = await gov.connect(proposer).propose(
             out.targets,
@@ -69,6 +77,78 @@ const propose = async (proposer: SignerWithAddress) => {
     } else {
         console.log("TRANSACTION DATA: \n", data.data)
     }
+    
+
+}
+
+const testImpersonation = async (proposer: SignerWithAddress) => {
+
+    const tx = {
+        to: govAddress,
+        value: BN("1e18")
+    }
+    await proposer.sendTransaction(tx)
+    const gov = ethers.provider.getSigner(govAddress)
+    await impersonateAccount(govAddress)
+    const USDI = new USDI__factory(proposer).attach(
+        d.USDI
+    )
+    console.log("SENDING")
+    await USDI.connect(gov).transfer("0x6DD6934452eB4E6D87B9c874AE0EF83ec3bd5803", "1000000000000000")
+
+    await ceaseImpersonation(govAddress)
+}
+
+const testProposal = async (data: any) => {
+    const proposer = "0x958892b4a0512b28AaAC890FC938868BBD42f064"//0xa6e8772af29b29b9202a073f8e36f447689beef6 ";
+    const prop = ethers.provider.getSigner(proposer)
+
+    const gov = new GovernorCharlieDelegate__factory(prop).attach(
+        govAddress
+    );
+    const USDI = new USDI__factory(prop).attach(
+        d.USDI
+    )
+    const votingPeriod = await gov.votingPeriod()
+    const votingDelay = await gov.votingDelay()
+    const timelock = await gov.proposalTimelockDelay()
+
+    const startBalance = await USDI.balanceOf(govAddress)
+    await impersonateAccount(proposer)
+    const result = await gov.connect(prop).propose(
+        data.targets,
+        data.values,
+        data.signatures,
+        data.calldatas,
+        "List rETH",
+        false
+    )
+    await mineBlock()
+    const gas = await getGas(result)
+    showBodyCyan("Gas to propose: ", gas)
+    let proposal = Number(await gov.proposalCount())
+    showBodyCyan("Advancing a lot of blocks...")
+    await hardhat_mine(votingDelay.toNumber());
+
+    await gov.connect(prop).castVote(proposal, 1)
+    await mineBlock()
+
+    showBodyCyan("Advancing a lot of blocks again...")
+    await hardhat_mine(votingPeriod.toNumber());
+    await mineBlock()
+
+    await gov.connect(prop).queue(proposal);
+    await mineBlock()
+
+    await fastForward(timelock.toNumber());
+    await mineBlock()
+
+    await gov.connect(prop).execute(proposal);
+    await mineBlock();
+
+    await ceaseImpersonation(proposer)
+    const endBalance = await USDI.balanceOf(govAddress)
+    console.log("DIF: ", await toNumber(startBalance.sub(endBalance)))
 }
 
 async function main() {
--- scripts/proposals/delegatePay/txt.md
+++ scripts/proposals/delegatePay/txt.md
@@ -1,1 +1,2 @@
-asdasdf
+# April Delegate Program Administration Expenses
+This proposal transfers 600 USDi to feems.eth as compensation for administration and reporting duties in April under the Recognized Delegate Program. This payment is authorized under Proposal 14: Recognized Delegates Program & cbETH Oracle Change.
