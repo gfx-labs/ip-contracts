6afb69e9317da8344a499b59b00786de97185fcd
Jake <jbarber@gfxlabs.io>
2023-08-14 13:08:18 -0500

    fixed proposal

--- scripts/proposals/govThresholds/propose.ts
+++ scripts/proposals/govThresholds/propose.ts
@@ -4,80 +4,136 @@ import {
   GovernorCharlieDelegate__factory, USDI__factory
 } from "../../../typechain-types";
 import { ProposalContext } from "../suite/proposal";
-import { d } from "../DeploymentInfo";
-import { reset } from "../../../util/block";
+import { currentBlock, fastForward, hardhat_mine, reset, resetCurrent } from "../../../util/block";
 import * as fs from 'fs';
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
+import hre from 'hardhat';
+import { showBodyCyan } from "../../../util/format";
+import { ceaseImpersonation } from "../../../util/impersonator";
 
 const { ethers, network, upgrades } = require("hardhat");
 
-const governorAddress = "0x266d1020A84B9E8B0ed320831838152075F8C4cA"
+const govAddress = "0x266d1020A84B9E8B0ed320831838152075F8C4cA"
 
-const feems = "0x6DD6934452eB4E6D87B9c874AE0EF83ec3bd5803"
+const proposeFromScript = false
+const proposerAddr = "0x958892b4a0512b28AaAC890FC938868BBD42f064"
 
-const newProposalThreshold = BN("200000e18")
-const newQuorum = BN("2000000e18")
-const newOptimisticQuorum = BN("500000e18")
+const propose = async (proposer: SignerWithAddress) => {
+
+  const proposal = new ProposalContext("GovThresholds")
+
+  const out = proposal.populateProposal()
+  const proposalText = fs.readFileSync('./scripts/proposals/govThresholds/txt.md', 'utf8');
+
+  let gov: GovernorCharlieDelegate;
+  gov = new GovernorCharlieDelegate__factory(proposer).attach(
+    govAddress
+  );
+
+  const data = await gov.connect(proposer).populateTransaction.propose(
+    out.targets,
+    out.values,
+    out.signatures,
+    out.calldatas,
+    proposalText,
+    false
+  )
+
+
+  if (proposeFromScript) {
+    console.log("Sending proposal")
+    const result = await gov.connect(proposer).propose(
+      ["0x0000000000000000000000000000000000000000"],//out.targets,
+      [BN("0")],
+      [""],
+      ["0x"],
+      proposalText,
+      false
+    )
+    const receipt = await result.wait()
+    console.log("Proposal sent: ", receipt.transactionHash)
+    const networkName = hre.network.name
+    if (networkName == "hardhat" || networkName == "localhost") {
+      //test execution if on test network 
+      console.log("Testing execution")
+      await quickTest(proposer)
+    }
+  } else {
+    console.log("TRANSACTION DATA: \n", data.data)
+    //fs.writeFileSync('./scripts/proposals/MKR/proposalHexData.txt', JSON.stringify(data))
+  }
+  console.log(out)
+}
+
+const quickTest = async (proposer: SignerWithAddress) => {
+  const gov = new GovernorCharlieDelegate__factory(proposer).attach(
+    govAddress
+  )
+
+  const votingPeriod = await gov.votingPeriod()
+  const votingDelay = await gov.votingDelay()
+  const timelock = await gov.proposalTimelockDelay()
+
+  const block = await currentBlock()
+
+  const proposal = Number(await gov.proposalCount())
+  showBodyCyan("Advancing a lot of blocks...")
+  await hardhat_mine(votingDelay.toNumber())
+
+  await gov.connect(proposer).castVote(proposal, 1)
+
+  await ceaseImpersonation(proposerAddr)
+  const whale = "0xa6e8772af29b29b9202a073f8e36f447689beef6"//"0x958892b4a0512b28AaAC890FC938868BBD42f064"//0xa6e8772af29b29b9202a073f8e36f447689beef6 ";
+  const prop = ethers.provider.getSigner(whale)
+  await impersonateAccount(whale)
+  await gov.connect(prop).castVote(proposal, 1)
+
+  showBodyCyan("Advancing a lot of blocks again...")
+  await hardhat_mine(votingPeriod.toNumber())
+
+  await gov.connect(prop).queue(proposal)
+
+  await fastForward(timelock.toNumber())
+
+  const result = await gov.connect(prop).execute(proposal)
+  await result.wait()
+  showBodyCyan("EXECUTION COMPLETE")
+
+  await ceaseImpersonation(whale)
+}
 
 async function main() {
-    //enable this for testing on hardhat network, disable for testnet/mainnet deploy
-    await reset(16579397)
+  let proposer: SignerWithAddress
+
+
+  const networkName = hre.network.name
+  if (networkName == "hardhat" || networkName == "localhost") {
     await network.provider.send("evm_setAutomine", [true])
+    //await resetCurrent()
+    await reset(17886985)
+    await impersonateAccount(proposerAddr)
+    proposer = ethers.provider.getSigner(proposerAddr)
+    console.log("TEST PROPOSAL AT BLOCK: ", await (await currentBlock()).number)
+
+  } else {
+    const accounts = await ethers.getSigners()
+    proposer = accounts[1]
+    if (proposeFromScript) {
+      console.log("PROPOSING ON MAINNET AS: ", proposer.address)
+    } else {
+      console.log("GENERATING PROPOSAL TRANSACTION DATA")
+    }
+  }
+
+  await propose(proposer)
 
 
-
-    const accounts = await ethers.getSigners();
-    const deployer = accounts[0];
-
-    const proposal = new ProposalContext("GovThresholds")
-
-    const setProposalThreshold = await new GovernorCharlieDelegate__factory(deployer).
-      attach(governorAddress).
-      populateTransaction._setProposalThreshold(
-        newProposalThreshold
-      )
-
-    const setQuorumThreshold = await new GovernorCharlieDelegate__factory(deployer).
-      attach(governorAddress).
-      populateTransaction._setQuorumVotes(
-        newQuorum
-      )
-
-    const setOptimisticQuorum = await new GovernorCharlieDelegate__factory(deployer).attach(governorAddress).populateTransaction._setOptimisticQuorumVotes(
-      newOptimisticQuorum
-    )
-
-    const transferUSDi = await new USDI__factory(deployer).attach(d.USDI).populateTransaction.transfer(feems, BN("600e18"))
-
-    proposal.addStep(setProposalThreshold, "_setProposalThreshold(uint256)")
-    proposal.addStep(setQuorumThreshold, "_setQuorumVotes(uint256)")
-    proposal.addStep(setOptimisticQuorum, "_setOptimisticQuorumVotes(uint256)")
-
-    proposal.addStep(transferUSDi, "transfer(address,uint256)")
-
-    const out = proposal.populateProposal()
-    const proposalText = fs.readFileSync('./scripts/proposals/govThresholds/txt.md', 'utf8');
-
-    let gov: GovernorCharlieDelegate;
-    gov = new GovernorCharlieDelegate__factory(deployer).attach(
-        governorAddress
-    );
-
-    const data = await gov.connect(deployer).populateTransaction.propose(
-        out.targets,
-        out.values,
-        out.signatures,
-        out.calldatas,
-        proposalText,
-        false
-    )
-
-    fs.writeFileSync('./scripts/proposals/govThresholds/proposalHexData.txt', JSON.stringify(data));
-    //console.log(JSON.stringify(data))
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
